8e745e841e06dce84c1027d6daea83f5052ba196
shelby <sunyoujie@xun-ao.com>
2009-06-13 16:13:36 +0800

    login

--- javascript/login.js
+++ javascript/login.js
@@ -1,13 +1,13 @@
 function login(param)
 {
   var p;
-  if($.cookie('smg_username')!="")
+  if($.cookie('smg_user_nickname')!="")
   {
-  	p=$.cookie('smg_username');	
+  	p=$.cookie('smg_user_nickname');	
   }
 	else
 	{	
-		p='<a href="login.frame.php?height=120&width=300&modal=true" class="thickbox" title="Please Sign In">µÇÂ¼</a>';
+		p='<a href="login.php?height=120&width=300&modal=true" class="thickbox" title="Please Sign In">µÇÂ¼</a>';
 	}	
 	
 	$('#'+param).html(p);
--- login/index.php
+++ login/index.php
@@ -2,14 +2,12 @@
 require_once('../frame.php');
 use_jquery();
 js_include_once_tag('thickbox');
-css_include_tag('thickbox');
 js_include_once_tag('login');
+css_include_tag('thickbox');
+js_include_once_tag('jquery.cookie');
 ?>
 
-
-<span id=login_context>
-</span>
+<span id=login_context name=login_context></span> 　
 <script language="javascript">
 login('login_context');
-	
 </script>
