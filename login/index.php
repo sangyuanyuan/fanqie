061aef80d3b70e890f5940a6e58323bc82613a4e
shelby <sunyoujie@xun-ao.com>
2009-06-15 10:23:58 +0800

    login

--- login/index.php
+++ login/index.php
@@ -2,12 +2,14 @@
 require_once('../frame.php');
 use_jquery();
 js_include_once_tag('thickbox');
-js_include_once_tag('login');
+js_include_once_tag('user');
 css_include_tag('thickbox');
 js_include_once_tag('jquery.cookie');
 ?>
 
-<span id=login_context name=login_context></span> 　
+<span id=login_context name=login_context></span>　
+<span id=logout_context name=logout_context onclick="check_logout('login_context','logout_context');">退出</span>
 <script language="javascript">
 login('login_context');
+logout('logout_context');
 </script>
