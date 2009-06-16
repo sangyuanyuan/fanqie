5edb7d7c266c967d17e92f19f6a664c8797c127c
justin1986 <zhanghao@xun-ao.com>
2009-06-16 20:51:37 +0800

    yun

--- admin/admin.php
+++ admin/admin.php
@@ -1,81 +1,64 @@
 <?php
 	require_once('../frame.php');
+	css_include_tag('admin');
+	use_jquery_ui();
 ?>
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv=Content-Type content="text/html; charset=utf-8">
 	<meta http-equiv=Content-Language content=zh-CN>
 	<title>smg</title>
-	<? 	
-		css_include_tag('admin');
-		use_jquery_ui();
-  ?>
 </head>
-<body style="background:url(/images/admin/bg.jpg) repeat-x;">
+<body  style="background:url(/images/bg/admin_bg2.jpg) repeat-x;">
 	<div id=admin_body>
 		<div id=part1>
-			<div id=nav>欢迎 </div>
-			<div id=title>番茄网后台管理系统</div>
-			<div id=index><a href="/index.php" target="_blank">动态主页</a></div>
+			<div id=nav></div>
+			<div id=title>SMG</div>
+			<div id=index><a href="/index.php" target="_blank">SMG</a></div>
 		</div>
 		<div id=part2>
-				<?php
+			<div id="accordion">
+				<?php				
 					$menu = new table_class("smg_admin_menu");
-					$main_menu = $menu->find("all",array('order' => 'parent_id,priority desc'));
-					$main_menu2 = $main_menu;
-					//--------------------				
-					for($i=count($main_menu)-1;$i>=0;$i--)
-					{
-
-						//--------------				
-						if(0==$main_menu[$i]->parent_id){ 
+					$main_menu = $menu->find("all",array('conditions' => 'href="#"  and parent_id=0','order' => 'priority'));
+					for($i=0;$i<count($main_menu);$i++){
 				?>
-<<<<<<< HEAD:admin/admin.php
-						<div class="menu1" id="list1_<?php echo $i?>" ><a href="<?php echo $main_menu[$i]->href;?>"><?php echo $main_menu[$i]->name;?></a></div>
-=======
-						<div class="menu1"><a href="<?php echo $main_menu[$i]->href;?>" target="<?php echo $main_menu[$i]->target;?>" list="<?php echo $i;?>"><?php echo $main_menu[$i]->name;?></a></div>
->>>>>>> 0346250b66509496be422d7d41f75a7330e35c1b:admin/admin.php
-						<? 
-							 //-----
-							 for($j=count($main_menu2)-1;$j>=0;$j--)
-							 {	
-							 		if($main_menu[$i]->id==$main_menu2[$j]->parent_id)
-							 		{
-						 ?>	 			
-						 			<div class="menu2 list2_<?php echo $i;?>" onClick='$("#admin_iframe").attr("src","<?php echo $main_menu2[$j]->href; ?>")' >.<?php echo $main_menu2[$j]->name ?></div>
-						 <?	 			
-							 		}
-						   }
-						   //-----
-						?>
-				<?php 
-						}
-						//--------------				
-					}
-				  //--------------------				
-				?>
+				<div class=menu1><a href="<?php echo $main_menu[$i]->href;?>"><?php echo $main_menu[$i]->name;?></a></div>
+				<div>
+					<?php
+						$child_menu=$menu->find("all",array('conditions' => 'parent_id='.$main_menu[$i]->id,'order' => 'priority'));
+						for($j=0;$j<count($child_menu);$j++){
+					?>
+						<div class=menu2>·<a href="<?php echo $child_menu[$j]->href;?>" target="admin_iframe"><?php echo $child_menu[$j]->name;?></a></div>
+					<?php }?>
+				</div>
+				<?php }?>
+			</div>
+			<?php
+				$main_menu = $menu->find("all",array('conditions' => 'href!="#" and parent_id=0','order' => 'priority'));
+				for($i=0;$i<count($main_menu);$i++){
+			?>
+			<div class=menu1><a href="<?php echo $main_menu[$i]->href;?>" target="admin_iframe"><?php echo $main_menu[$i]->name;?></a></div>
+			<?php }?>
 		</div>
 		
 		<div id=part3>
-		  <iframe id=admin_iframe name="admin_iframe" scrolling="yes" src="/admin/menu/menu_list.php" width="99%" height="700"></iframe>
-		</div>		
+		  <iframe id=admin_iframe name="admin_iframe" scrolling="yes" src="menu_list.php" width="99%" height="700"></iframe>
+		</div>
 	</div>
 </body>
 </html>
-<script>
-$(function(){
-	$(".menu1 a").click(function(e){
-		if($(this).attr("target")=="#")
-		{
-		   e.preventDefault();
-		   $(".menu2").hide();
-		   $(".list2_"+$(this).attr("list")).show();
-		}
-	});
-	
-});
-</script>
 
+<SCRIPT type=text/javascript>
+	$(function() {
+		$("#accordion").accordion({
+			active:10,
+			autoHeight:false,
+			animated:false
+		});
+	});
+</SCRIPT>
 
 
