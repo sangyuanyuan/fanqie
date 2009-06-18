f849108563c38c2869c558d0762bdb6c4efbfbd0
justin1986 <zhanghao@xun-ao.com>
2009-06-18 17:45:22 +0800

    edit login.php

--- login/login.php
+++ login/login.php
@@ -21,7 +21,7 @@ require_once('../frame.php');
 			<div id=box style="border:1px solid #0066FF">
 				<div id=name>用户名　　<input type="text" id=admin_username name=admin_username style="width:145px; height:17px;"></div>
 				<div id=pwd>密　码　　<input type="password" id=admin_password name=admin_password onKeyPress="Press_Login()" style="width:145px; height:17px;"></div>
-				<div id=btn><input type="checkbox" id=nickname >昵称　　<a href="register.php" style="color:#000000; text-decoration:none">注册</a>　　<input type="button" value="登录" onClick="Admin_Login()" class="btn"></div>	
+				<div id=btn><input type="checkbox" id=nickname >昵称　　<a href="register.php" style="color:#000000; text-decoration:none">注册</a>　　<input type="button" value="登录" onClick="Admin_Login()" class="botton"></div>	
 				<input type="hidden" name="lasturl" value="<?php echo $lasturl;?>">
 				<div id=zhu>“番茄网”登录指南：<br>・ 请使用您的“一卡通”工号与口令登录“番茄网”<br>・ 如果您的Windows是用工号登录的，则登录“番茄网”的口令与Windows的登录口令一样<br>・ 如果您从未使用过工号进行过登录，默认口令为“Password@1”（P大写）<br>・ 如果您多次尝试还不能登录或遗忘了口令，请致电1000分机。</div>	
 			</div>
