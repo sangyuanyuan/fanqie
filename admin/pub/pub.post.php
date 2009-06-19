c9edff82c06ef79c839cdadacadbf3b9dbd22883
shelby <sunyoujie@xun-ao.com>
2009-06-19 16:15:05 +0800

    deptlist

--- admin/pub/pub.post.php
+++ admin/pub/pub.post.php
@@ -16,4 +16,20 @@
 		redirect($_POST['url']);
 		
 	}
+	elseif("edit_priority"==$_POST['post_type'])
+	{
+		$post = new table_class($_POST['db_table']);
+		$id_str=explode("|",$_POST['id_str']); 
+		$priority_str=explode("|",$_POST['priority_str']); 
+		$id_str_num=sizeof($id_str)-1;
+		for($i=$id_str_num-1;$i>=0;$i--)
+		{
+			if($priority_str[$i]==""){$priority_str[$i]="100";}
+			$db = get_db();
+			$sql="update smg_dept_list set priority=".$priority_str[$i]." where id=".$id_str[$i];
+			$db->execute($sql);
+		}		
+	}
+	
+	
 ?>
