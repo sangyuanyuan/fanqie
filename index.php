7c32201ba05c51a6556dbd625410a090ee2fbacb
shengzhifeng <shengzhifeng@xun-ao.com>
2009-06-22 17:56:07 +0800

    css

--- index.php
+++ index.php
@@ -14,17 +14,63 @@
 	 <div id=bottom>
 	 	<div id=title><input type="text"></div>
 		<div id=left>
-			<div class=topthree style="background:url('/images/bg/bottom_bg.jpg') no-repeat;"></div>
-			<div class=topthree style="background:url('/images/bg/bottom_bg1.jpg') no-repeat;"></div>
-			<div class=topthree style="background:url('/images/bg/bottom_bg2.jpg') no-repeat;"></div>
-			<div class=bottomthree style="background:url('/images/bg/bottom_bg3.jpg') no-repeat;"></div>
-			<div class=bottomthree style="background:url('/images/bg/bottom_bg4.jpg') no-repeat;"></div>
-			<div class=bottomthree style="background:url('/images/bg/bottom_bg5.jpg') no-repeat;"></div>
+			<div class=topthree style="background:url('/images/bg/bottom_bg.jpg') no-repeat;">
+				<div class="first"><a target="_blank" href="#">test</a></div>
+				<? for($i=0;$i<5;$i++){?>
+					<div class="cl"><img src="images/pic/fangkuai.jpg"><a target="_blank" href="#">test</a></div>
+				<? }?>
+			</div>
+			<div class=topthree style="background:url('/images/bg/bottom_bg1.jpg') no-repeat;">
+				<div class="first"><a target="_blank" href="#">test</a></div>
+				<? for($i=0;$i<5;$i++){?>
+					<div class="cl"><img src="images/pic/fangkuai.jpg"><a target="_blank" href="#">test</a></div>
+				<? }?>
+			</div>
+			<div class=topthree style="background:url('/images/bg/bottom_bg2.jpg') no-repeat;">
+				
+				
+			</div>
+			<div class=bottomthree style="background:url('/images/bg/bottom_bg3.jpg') no-repeat;">
+				<div class="first"><a target="_blank" href="#">test</a></div>
+				<? for($i=0;$i<5;$i++){?>
+					<div class="cl"><img src="images/pic/fangkuai.jpg"><a target="_blank" href="#">test</a></div>
+				<? }?>
+				<a target="_blank" href="#"><img class=pic src="images/pic/bottom.jpg"></a>
+			</div>
+			<div class=bottomthree style="background:url('/images/bg/bottom_bg4.jpg') no-repeat;">
+				<div class="first"><a target="_blank" href="#">test</a></div>
+				<? for($i=0;$i<7;$i++){?>
+					<div class="cl"><img src="images/pic/fangkuai.jpg"><a target="_blank" href="#">test</a></div>
+				<? }?>
+			</div>
+			<div class=bottomthree style="background:url('/images/bg/bottom_bg5.jpg') no-repeat;">
+				<div class="first"><a target="_blank" href="#">test</a></div>
+				<? for($i=0;$i<5;$i++){?>
+					<div class="cl"><img src="images/pic/fangkuai.jpg"><a target="_blank" href="#">test</a></div>
+				<? }?>
+				<a target="_blank" href="#"><img class=pic src="images/pic/bottom.jpg"></a>
+			</div>
 		</div>
 		<div id=right>
-			<div class=bottomright></div>
-			<div class=bottomright></div>
-			<div id=rightbottom></div>
+			<div class=bottomright>
+				<a target="_blank" style="color:#FFFF33" href="#">　　我型我秀</a><a target="_blank" href="#">　　绘　画</a><a target="_blank" href="#">　　摄　影</a><a target="_blank" href="#">　　书　法</a>
+				<div class=content>
+					<div class=pic><a target="_blank" href="#"><img border=0 src="images/pic/right.jpg"></a></div>
+					<? for($i=0;$i<4;$i++){?>
+						<div class=cl><a target="_blank" href="#"><img border=0 src="images/icon/icon1.jpg"> test</a></div>
+					<? }?>
+				</div>
+			</div>
+			<div class=bottomright>
+				<a target="_blank" style="color:#FFFF33" href="#">　　 杂　志</a><a target="_blank" href="#">　　 工　具</a><a target="_blank" href="#">　 生活帮帮</a><a target="_blank" href="#">　 网络链接</a>
+				<div class=content>
+					<div class=pic1><a target="_blank" href="#"><img border=0 src="images/pic/rightbottom.jpg"></a></div>
+					<div class=cl1><a target="_blank" href="#"><span style="color:#FF6600">test</span><br>test<br>test<br>test<br>test</a></div>
+					<div class=pic1><a target="_blank" href="#"><img border=0 src="images/pic/rightbottom.jpg"></a></div>
+					<div class=cl1><a target="_blank" href="#"><span style="color:#FF6600">test</span><br>test<br>test<br>test<br>test</a></div>
+				</div>
+			</div>
+			<div id=rightbottom><span style="margin-top:10px; margin-left:50px; font-size:14px; font-weight:bold; color:#FFE808; float:left; display:inline;">番茄视听</span><a style="width:88px; margin-left:38px;" target="_blank" href="#">众星闪耀第一</a><a style="margin-left:23px;" target="_blank" href="#">众星闪耀第二</a></div>
 		</div>
 	 </div>
 	</div>
