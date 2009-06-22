b7205ad6d6b049f6cefa3fdf94d35065934fd133
justin1986 <zhanghao@xun-ao.com>
2009-06-22 15:55:29 +0800

    if priority=100 in _add,_edit then hide the priority value

--- admin/magazine/magazine_add.php
+++ admin/magazine/magazine_add.php
@@ -1,7 +1,7 @@
 <?php
 	require_once('../../frame.php');
 	$category = new table_class("smg_category");
-	$category_menu = $category->find("all",array('conditions' => "category_type='magazine' and parent_id>0"));
+	$category_menu = $category->find("all",array('conditions' => "category_type='magazine' and parent_id>0","order" => "priority"));
 	//上述查询语句条件是类型是电子杂志父类不是4种大类并且该类是可发布的
 ?>
 
