e0c1304ccbf5eda81b735603d6fa7c5a5ac6000c
shengzhifeng <shengzhifeng@xun-ao.com>
2009-06-22 15:18:29 +0800

    css

--- index.php
+++ index.php
@@ -3,90 +3,30 @@
 <head>
 	<meta http-equiv=Content-Type content="text/html; charset=utf-8">
 	<meta http-equiv=Content-Language content=zh-CN>
-	<title>css123</title>
+	<title>index</title>
 	<link href="/css/top.css" rel="stylesheet" type="text/css">
 	<link href="/css/index.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 	<div id=index>
-		<div id=bottom>
-			<div id=left>
-				<div class=topthree style="background:url('images/bottom_bg.jpg') no-repeat;">
-					
-				</div>
-			</div>
-		</div>
-	<div id=nav1>
-		<div id=topimg1>
-		   	<img src="/images/top/sm.jpg" width=113 height=24>
-		</div>
-		<div id=topimg1>
-		   	<img src="/images/top/tools.jpg" width=61 height=24>
-		</div>
-		<div id=toptext1>
-		   	企业邮箱 新节目 直播 联系我们 常用办公软件
-		</div>
-	</div>
-	<!--上面灰色条
-	//下面分类导航-->
-	<div id=nav2>
-	 <div id=box>	
-		<div id=logo>
-	  </div>
-		<ul>
-			<li><b>最新</b></li>
-			<li>市场</li>
-			<li>直播</li>
-			<li>理财</li>
-			<li><b>要点</b></li>
-			<li>新闻</li>
-			<li>社会</li>
-			<li>SMG</li>
-	  </ul>	
-	  <ul>
-			<li><b>新闻</b></li>
-			<li>视频</li>
-			<li>公告</li>
-			<li>专题</li>
-			<li><b>爆料</b></li>
-			<li>业务</li>
-			<li>群团</li>
-			<li>简讯</li>
-	  </ul>	
-	  <ul>
-			<li><b>论坛</b></li>
-			<li>对话</li>
-			<li>旅游</li>
-			<li>热点</li>
-			<li><b>博客</b></li>
-			<li>聊天</li>
-			<li>影评</li>
-			<li>DIY</li>
-	  </ul>	
-	  <ul>
-			<li><b>摄影</b></li>
-			<li>活动</li>
-			<li>宝宝</li>
-			<li>音频</li>
-			<li><b>视频</b></li>
-			<li>杂志</li>
-			<li>每周之星</li>
-	  </ul>
-	  <ul>
-			<li><b>团购</b></li>
-			<li>网店</li>
-			<li>生日</li>
-			<li>题库</li>
-			<li><b>工具</b></li>
-			<li>菜单</li>
-			<li>投票</li>
-			<li>婚介</li>
-	  </ul>		
 		
-					
-<select><option>部门列表</option></select>
-	  
-	 </div> 
+	 <div id=top></div>
+	 <div id=bottom>
+	 	<div id=title><input type="text"></div>
+		<div id=left>
+			<div class=topthree style="background:url('/images/bg/bottom_bg.jpg') no-repeat;"></div>
+			<div class=topthree style="background:url('/images/bg/bottom_bg1.jpg') no-repeat;"></div>
+			<div class=topthree style="background:url('/images/bg/bottom_bg2.jpg') no-repeat;"></div>
+			<div class=bottomthree style="background:url('/images/bg/bottom_bg3.jpg') no-repeat;"></div>
+			<div class=bottomthree style="background:url('/images/bg/bottom_bg4.jpg') no-repeat;"></div>
+			<div class=bottomthree style="background:url('/images/bg/bottom_bg5.jpg') no-repeat;"></div>
+		</div>
+		<div id=right>
+			<div class=bottomright></div>
+			<div class=bottomright></div>
+			<div id=rightbottom></div>
+		</div>
+	 </div>
 	</div>
 </body>
 </html>
