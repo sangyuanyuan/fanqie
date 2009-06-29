95e091729ea035ddb6f6c02110a55cbb98be2029
justin1986 <zhanghao@xun-ao.com>
2009-06-29 16:11:58 +0800

    smg_vote 1.0

--- admin/vote/vote.post.php
+++ admin/vote/vote.post.php
@@ -1,6 +1,6 @@
 <?php
 	require_once "../../frame.php";
-	//var_dump($_POST['vote']);
+	//var_dump($_POST);
 	
 	if("del"==$_POST['post_type'])
 	{
@@ -12,8 +12,11 @@
 		echo $_POST['del_id'];
 	}else{
 		$vote = new table_class('smg_vote');
-		
-		if($_FILES['image']!=null){
+		if($_POST['type']=='edit'){
+			$vote->find($_POST['vote_id']);
+			$vote->echo_sql = true;
+		}
+		if($_FILES['image'][name]!=null){
 			$upload = new upload_file_class();
 			$upload->save_dir = '/upload/images/';
 			$img = $upload->handle('image','filter_pic');
@@ -25,28 +28,50 @@
 		}//如果投票上传图片，做处理
 		
 		$vote->update_attributes($_POST['vote']);
-		for($i=1;$i<=$_POST['vote_item_count'];$i++){
-			$vote_item = new table_class('smg_vote_item');
-			$vote_item->vote_id = $vote->id;
-			if($_POST['vote']['vote_type']=='image_vote'){
-				$upload = new upload_file_class();
-				$upload->save_dir = '/upload/images/';
-				$img = $upload->handle('item_image'.$i,'filter_pic');
-				//var_dump($_FILES);
-				
-				if($img === false){
-					alert('上传文件失败 !');				
-					redirect('vote_add.php');
-				}
-				
-				$vote_item->photo_url = "/upload/images/" .$img;
-			}//投票项目图片处理
-			
-			$vote_item->update_attributes($_POST['vote_item'.$i]);
-		}
 		
+		for($i=1;$i<=$_POST['vote_item_count'];$i++){
+			if($_POST['deleted'.$i]=='false'){
+				$vote_item = new table_class('smg_vote_item');
+				if(null!==$_POST['vote_id']){
+					$vote_item->find($_POST['vote_item'.$i.'_id']);
+				}
+				$vote_item->vote_id = $vote->id;
+				if($_POST['vote']['vote_type']!='more_vote'){
+					if($_FILES['item_image'.$i]['name']!=null){
+						$upload = new upload_file_class();
+						$upload->save_dir = '/upload/images/';
+						$img = $upload->handle('item_image'.$i,'filter_pic');
+						//var_dump($_FILES);
+						
+						if($img === false){
+							alert('上传文件失败 !');				
+							redirect('vote_add.php');
+						}
+						
+						$vote_item->photo_url = "/upload/images/" .$img;
+					}//投票项目图片处理
+					
+					$vote_item->update_attributes($_POST['vote_item'.$i]);
+				}else{
+					$vote_item->sub_vote_id = $_POST['vote_id'.$i];
+					$vote_item->save();
+				}
+			}else{
+				if($_POST['vote']['vote_type']=='more_vote'){
+					$vote_item = new table_class('smg_vote');
+					$vote_item -> delete($_POST['vote_id'.$i]);
+					$db = get_db();
+					$sql = 'delete from smg_vote_item where vote_id='.$_POST['vote_id'.$i];
+					$sql2 = 'delete from smg_vote_item where sub_vote_id='.$_POST['vote_id'.$i];
+					echo $sql2;
+					$db->execute($sql);
+					$db->execute($sql2);
+				}else{
+					$vote_item = new table_class('smg_vote_item');
+					$vote_item -> delete($_POST['vote_item'.$i.'_id']);
+				}
+			}
+		}
 		redirect('vote_list.php');
-	}
-	
-	
+	}	
 ?>
