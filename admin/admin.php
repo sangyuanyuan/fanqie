ab6e07cb0c34decb5ce352e56a424a7718a61ac8
justin1986 <zhanghao@xun-ao.com>
2009-07-05 13:30:59 +0800

    a

--- admin/admin.php
+++ admin/admin.php
@@ -1,6 +1,6 @@
 <?php
 	require_once('../frame.php');
-	//require_role();
+	require_role('admin');
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -16,7 +16,7 @@
 <body style="background:url(/images/admin/bg.jpg) repeat-x;">
 	<div id=admin_body>
 		<div id=part1>
-			<div id=nav>欢迎 </div>
+			<div id=nav>欢迎 <?php echo $_COOKIE['smg_role']; ?></div>
 			<div id=title>番茄网后台管理系统</div>
 			<div id=index><a href="/index.php" target="_blank">动态主页</a></div>
 		</div>
