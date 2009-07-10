0f1cf446cf4be58c8183d467eef8875f13c103fd
robbin17 <robbin_szf@hotmail.com>
2009-07-10 16:12:32 +0800

    css

---
+++ server/marry.php
@@ -0,0 +1,36 @@
+<?php
+	require_once('../frame.php');
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+	<meta http-equiv=Content-Language content=zh-cn>
+	<title>SMG-番茄网-服务-婚庆</title>
+	<? 	
+		css_include_tag('server_marry','top','bottom');
+		use_jquery();
+  ?>
+	
+</head>
+<body>
+<? require_once('../inc/top.inc.html');?>
+<div id=ibody>
+	<div id=ibody_top>
+		<div id=t_t></div>
+		<div id=t_l></div>
+		<div id=t_r></div>
+	</div>
+	<div id=ibody_middle></div>
+	<div id=ibody_line></div>
+	<div id=ibody_bottom>
+		<div class=b1></div>
+		<div class=b2></div>
+		<div class=b1></div>
+		<div class=b2></div>
+	</div>
+</div>
+<? require_once('../inc/bottom.inc.php');?>
+
+</body>
+</html>
