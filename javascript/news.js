52ec9783cd114be3e4d82cca7b2bd754efa35c51
robbin17 <robbin_szf@hotmail.com>
2009-07-15 13:45:00 +0800

    css

--- javascript/news.js
+++ javascript/news.js
@@ -1,6 +1,6 @@
 	$(document).ready(function(){
 		display_fqbq('fqbq','comment');
-		updatenews(document.getElementById('newsid').value);
+		updatenews();
 	})
 	$("#r_t").click(function(){location.href="news_sub.php";})
 	function ChangeTab(num)
@@ -31,42 +31,16 @@
 		document.getElementById("b_b_"+num).style.display='block';	
 	}
 	function digg(type,id){
-		$.ajax({
-			type: "POST",
-			url: "news_digg.post.php",
-			timeout: 20000,
-			error: function(){
-				alert('error');
-			},
-			data: "type=" + type + "&comment_id=" + id ,
-			success: function(msg){
+		$.post('news_digg.post.php',{'type':type,'comment_id':id},function(data){
 			
-				//当AJAX请求失败时添加一个被执行的方法
-				if(msg!='OK'){
-					alert('刷新太快了请稍后再试！');
-				}else{
-					alert('评论成功！');
-				}
 			}
-		})
+		)
 	}
-	function updatenews(id){
-		$.ajax({
-			type: "POST",
-			url: "news_update.post.php",
-			timeout: 20000,
-			error: function(){
-				alert('error');
-			},
-			data: "newsid="+id ,
-			success: function(msg){
-				//当AJAX请求失败时添加一个被执行的方法
-				if(msg!='OK'){
-					alert('失败');
-				}else{
-					alert('成功');
-				}
+	function updatenews(){
+		$.post('news_update.post.php',{'newsid':$("#newsid").attr('value')},function(data){
+			
 			}
-		})
+		)
 	}
+
 	
