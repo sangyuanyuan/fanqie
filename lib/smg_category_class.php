43ac48b33eb2e6fa82c040d7e6e179e7e3a152b7
sauger <sauger@163.com>
2009-07-26 17:10:46 +0800

    fix bugs

--- lib/smg_category_class.php
+++ lib/smg_category_class.php
@@ -30,12 +30,12 @@ class smg_category_class
 			$table = new table_class('smg_category_dept');
 			if(empty($name)){
 				if(empty($type)){
-					$items = $table->find('all',array('conditions' => 'dept_id='.$dept_id));
+					$items = $table->find('all',array('conditions' => 'dept_id='.$dept_id,'order' => 'platform ,priority'));
 				}else{
-					$items = $table->find('all',array('conditions' => "category_type = '" .$type ."' and dept_id=".$dept_id));
+					$items = $table->find('all',array('conditions' => "category_type = '" .$type ."' and dept_id=".$dept_id,'order' => 'platform ,priority'));
 				}
 			}else{
-				$items = $table->find('all',array('conditions' => "name = '" .$name ."' and dept_id=".$dept_id));
+				$items = $table->find('all',array('conditions' => "name = '" .$name ."' and dept_id=".$dept_id,'order' => 'platform ,priority'));
 			}
 			
 			if($items){
