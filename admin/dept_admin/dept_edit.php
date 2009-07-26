c978df2e178fc95b361c63845845bf7d026cdbc6
sauger <sauger@163.com>
2009-07-26 11:05:04 +0800

    fix bugs

--- admin/dept_admin/dept_edit.php
+++ admin/dept_admin/dept_edit.php
@@ -14,29 +14,31 @@
 		$dept = new table_class('smg_dept');
 		$dept= $dept->find($_REQUEST['dept_id']);
 	?>
-	<table width="795" border="0" id="list">
-		<tr class="tr1">
-			<td colspan="4" align=left>　编辑部门</td>
-		</tr>
-		<tr class="tr2">
-			<td width="100">部门名称</td><td  align=left><input type="text" name="dept[name]" value="<?php echo $dept->name;?>"></td>
-		</tr>
-		<tr class="tr2">
-			<td width="100">优先级</td><td align=left><input type="text" name="dept[priority]" value="<?php echo $dept->priority;?>"></td>
-		</tr>
-		<tr class="tr2">
-			<td width="100">部门代码</td><td align=left><input type="text" name="dept[code]" value="<?php echo $dept->code;?>"></td>
-		</tr>
-		<tr class="tr2">
-			<td width="100">部门链接</td><td align=left><input type="text" name="dept[url]" value="<?php echo $dept->url;?>"></td>
-		</tr>
-		<tr class="tr2">
-			<td width="100">简短描述</td><td align=left><textarea name="dept[name]"><?php echo $dept->description;?></textarea></td>
-		</tr>
-		<tr class="tr2">
-			<td width="100" colspan=2><input type="submit" value="提交"></td>
-		</tr>
-	</table>
+	<form method="post" action="dept.post.php">
+		<table width="795" border="0" id="list">
+			<tr class="tr1">
+				<td colspan="4" align=left>　编辑部门</td>
+			</tr>
+			<tr class="tr2">
+				<td width="100">部门名称</td><td  align=left><input type="text" name="dept[name]" value="<?php echo $dept->name;?>"></td>
+			</tr>
+			<tr class="tr2">
+				<td width="100">优先级</td><td align=left><input type="text" name="dept[priority]" value="<?php echo $dept->priority;?>"></td>
+			</tr>
+			<tr class="tr2">
+				<td width="100">部门代码</td><td align=left><input type="text" name="dept[code]" value="<?php echo $dept->code;?>"></td>
+			</tr>
+			<tr class="tr2">
+				<td width="100">部门链接</td><td align=left><input type="text" name="dept[url]" value="<?php echo $dept->url;?>"></td>
+			</tr>
+			<tr class="tr2">
+				<td width="100">简短描述</td><td align=left><textarea name="dept[name]"><?php echo $dept->description;?></textarea></td>
+			</tr>
+			<tr class="tr2">
+				<td width="100" colspan=2><input type="submit" value="提交"></td>
+			</tr>
+		</table>
+	</form>
 </body>
 </html>
 
