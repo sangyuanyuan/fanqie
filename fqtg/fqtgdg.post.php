121568dafeed64dff1c6b0d6b64f7ff62658fe5f
robbin17 <robbin_szf@hotmail.com>
2009-07-27 17:52:43 +0800

    robbin

--- fqtg/fqtgdg.post.php
+++ fqtg/fqtgdg.post.php
@@ -3,7 +3,7 @@ require_once('../frame.php');
 $db=get_db();
 	$StrSql='insert into smg_tg_signup(tg_id,name,spname,num,phone,address,createtime,remark,dept_id) values ('.$_POST['tg_id'].',"'.$_POST['buyname'].'","'.$_POST['spname'].'",'.$_POST['num'].',"'.$_POST['phone'].'","'.$_POST['address'].'",now(),"'.$_POST['remark'].'",'.$_POST['deptid'].')';
 	$Record = $db->execute($StrSql);
-alert("提交成功！");
+alert("订购成功！");
 redirect('/fqtg/fqtgdg.php?id='.$_POST['tg_id']);
 exit;
 ?>
--- shop/spdg.post.php
+++ shop/spdg.post.php
@@ -3,7 +3,7 @@ require_once('../frame.php');
 $db=get_db();
 	$StrSql='insert into smg_shop_signup(tg_id,name,spname,num,phone,address,createtime,remark) values ('.$_POST['tg_id'].',"'.$_POST['buyname'].'","'.$_POST['spname'].'",'.$_POST['num'].',"'.$_POST['phone'].'","'.$_POST['address'].'",now(),"'.$_POST['remark'].'")';
 	$Record = $db->execute($StrSql);
-alert("提交成功！");
+alert("订购成功！");
 redirect('/shop/spdg.php?id='.$_POST['tg_id']);
 exit;
 ?>
