57b0d925c5770e1431d9e593aaa3f9555bd803be
robbin17 <robbin_szf@hotmail.com>
2009-07-27 11:33:00 +0800

    robbin

--- admin/tg/tginsert.php
+++ admin/tg/tginsert.php
@@ -5,7 +5,7 @@ include('../../frame.php');
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<meta http-equiv=Content-Type content="text/html; charset=gb2312">
+	<meta http-equiv=Content-Type content="text/html; charset=utf-8">
 	<meta http-equiv=Content-Language content=zh-CN>
 	<title>SMG</title>
 	<?php css_include_tag('admin','top','bottom');
--- admin/tg/tglist.php
+++ admin/tg/tglist.php
@@ -17,7 +17,7 @@ $count=$db->query($total);
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<meta http-equiv=Content-Type content="text/html; charset=gb2312">
+	<meta http-equiv=Content-Type content="text/html; charset=utf-8">
 	<meta http-equiv=Content-Language content=zh-CN>
 	<title>SMG -团购后台</title>
 	<link href="/css/admin.css" rel="stylesheet" type="text/css">
