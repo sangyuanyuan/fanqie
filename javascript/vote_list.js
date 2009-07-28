10b4e190b1b4df79c09b79feff2fbe4cabd672ca
justin1986 <zhanghao@xun-ao.com>
2009-07-28 17:53:36 +0800

    show

--- javascript/vote_list.js
+++ javascript/vote_list.js
@@ -15,4 +15,10 @@ $(function(){
 	$("#vote_search").click(function(){
 				window.location.href="?key="+$("#search_text").attr('value');
 	});
+	
+	$('#search_text').keydown(function(e){
+		if(e.keyCode == 13){
+			window.location.href="?key="+ encodeURI($("#search_text").attr('value'));			
+		}
+	});
 });
