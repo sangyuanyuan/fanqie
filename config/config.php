b091a10820eca51d1286e6e03065a71bc54ef54f
robbin17 <robbin_szf@hotmail.com>
2009-08-05 16:58:08 +0800

    robbin

--- config/config.php
+++ config/config.php
@@ -2,14 +2,14 @@
 /*
  * system config file
  */
- $debug_tag = false;
+ $debug_tag = true;
  
  /*
   * database configuration
   */
  
- $db_server_name = 'localhost';
- $db_database_name = 'enjoyoung_production';
+ $db_server_name = '172.27.203.82';
+ $db_database_name = 'smg_new';
  $db_user_name = 'root';
  $db_password = 'xunao';
  $db_code = 'utf8';
@@ -19,4 +19,7 @@
  $db_user_name_bak = 'root';
  $db_password_bak = '';
  $db_code_bak = 'utf8';
+ $g_news_tags = array('小编加精','公告','业务','群团','小编推荐','历史头条','小编推荐');
+ $g_video_tags = array('视频推荐');
+ $g_db_log_file = "d:\\tmp.txt";
 ?>
