137a40c71bc866665489802667d1c685935b6509
robbin17 <robbin_szf@hotmail.com>
2009-08-06 15:17:05 +0800

    robbin

--- subject/pj/index.php
+++ subject/pj/index.php
@@ -336,9 +336,9 @@ html,body{
           <TD ><div style="padding-top:5px;" class=index_title>番茄果农排行　　　<a target="_blank" href="/news/newslist.php?id=115"></a></div></TD></TR>
  			<? 
 			$strsql1='select *,(n1+v1+p1) as a1,(n2+v2+p2) as a2  from (select a.name,ifnull(b.allcounts,0) as n1,ifnull(c.counts,0) as n2,ifnull(p1allcounts,0) as p1,ifnull(p2counts,0) as p2,ifnull(v1allcounts,0) as v1,ifnull(v2counts,0) as v2 from smg_dept a left join
-(select count(dept_id) as allcounts,dept_id from smg_news where is_recommend=1  '.$sql.'  group by dept_id) b on a.id=b.dept_id left join  (select count(dept_id) as counts,dept_id from smg_news where is_adopt=1  '.$sql.' group by dept_id) c on b.dept_id = c.dept_id
-left join (select count(dept_id) as p1allcounts,dept_id from smg_images where is_recommend=1  '.$sql2.'  group by dept_id) p1 on a.id=p1.dept_id left join  (select count(dept_id) as p2counts,dept_id from smg_images where is_adopt=1 '.$sql2.'  group by dept_id) p2 on p1.dept_id = p2.dept_id
-left join (select count(dept_id) as v1allcounts,dept_id from smg_video where is_recommend=1  '.$sql2.'  group by dept_id) v1 on a.id=v1.dept_id left join  (select count(dept_id) as v2counts,dept_id from smg_video where is_adopt=1  '.$sql2.' group by dept_id) v2 on v1.dept_id = v2.dept_id
+(select count(dept_id) as allcounts,dept_id from smg_news n where is_recommend=1  '.$sql.'  group by dept_id) b on a.id=b.dept_id left join  (select count(dept_id) as counts,dept_id from smg_news n where is_adopt=1  '.$sql.' group by dept_id) c on b.dept_id = c.dept_id
+left join (select count(dept_id) as p1allcounts,dept_id from smg_images n where is_recommend=1  '.$sql2.'  group by dept_id) p1 on a.id=p1.dept_id left join  (select count(dept_id) as p2counts,dept_id from smg_images n where is_adopt=1 '.$sql2.'  group by dept_id) p2 on p1.dept_id = p2.dept_id
+left join (select count(dept_id) as v1allcounts,dept_id from smg_video n where is_recommend=1  '.$sql2.'  group by dept_id) v1 on a.id=v1.dept_id left join  (select count(dept_id) as v2counts,dept_id from smg_video n where is_adopt=1  '.$sql2.' group by dept_id) v2 on v1.dept_id = v2.dept_id
 order by b.allcounts desc) tb order by a1 desc';
 $clickcount=$db->query($strsql1);
 ?>
