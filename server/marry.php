90627068e25836f70f159c210764261a86b530bf
sauger <sauger@163.com>
2009-08-07 17:33:10 +0800

    marry

--- server/marry.php
+++ server/marry.php
@@ -14,11 +14,7 @@
 	
 </head>
 <script>
-<<<<<<< HEAD:server/marry.php
 total("婚庆","server");	
-=======
-	total("婚庆","server");	
->>>>>>> 2e2de592928ec1b823a14bbbe1c1861ff98236f4:server/marry.php
 </script>
 <body>
 <?php 
