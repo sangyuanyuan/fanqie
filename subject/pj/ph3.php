46b0ecb6be55245f73effafbcc0bc28d989a08d1
robbin17 <robbin_szf@hotmail.com>
2009-08-07 11:50:55 +0800

    robbin

--- subject/pj/ph3.php
+++ subject/pj/ph3.php
@@ -1,10 +1,10 @@
-<?php require_once('../../frame.php');
+﻿<?php require_once('../../frame.php');
 	$db=get_db();
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<meta http-equiv=Content-Type content="text/html; charset=gb2312">
+	<meta http-equiv=Content-Type content="text/html; charset=utf-8">
 	<meta http-equiv=Content-Language content=zh-CN>
 	<title>SMG</title>
 	<?php css_include_tag('smg','top','bottom'); ?>  
