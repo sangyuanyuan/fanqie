ff85ab9a0e2743affdee3ea4807c6f39caa10df0
justin1986 <zhanghao@xun-ao.com>
2009-08-08 11:19:40 +0800

    sivv

--- admin/test2.php
+++ admin/test2.php
@@ -1,11 +1,11 @@
 <?php
     require_once('../frame.php');
 	$db = get_db();
-	$sql = 'select dept_id,dept_category_id,title,created_at from smg_new_debug where dept_category_id!=0';
+	$sql = 'select dept_id,dept_category_id,created_at from smg_news_debug';
 	$record = $db->query($sql);
 	$count = count($record);
 	for($i=0;$i<$count;$i++){
-		$sql = 'update smg_news set dept_category_id='.$record[$i]->dept_category_id.' where dept_id='.$record[$i]->dept_id.' and title="'.$record[$i]->title.'" and created_at='.$record[$i]->created_at;
+		$sql = 'update smg_news set dept_category_id='.$record[$i]->dept_category_id.' where dept_id='.$record[$i]->dept_id.' and created_at="'.$record[$i]->created_at.'"';
 		$db->execute($sql);
 	}
 ?>
