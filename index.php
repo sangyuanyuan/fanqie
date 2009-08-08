66ef7483b689ca055ef6d2bb339c6d26c8ef6001
justin1986 <zhanghao@xun-ao.com>
2009-08-08 19:31:56 +0800

    show

--- index.php
+++ index.php
@@ -839,10 +839,14 @@ total("首页","other");
 						</div></TD>
 				                <? }?>
 				              </TR></TBODY></TABLE></TD>
-				          			<TD id=demo8 vAlign=top></TD></TR></TBODY></TABLE></DIV>
+				          			<TD id="demo8" vAlign=top></TD></TR></TBODY></TABLE></DIV>
 								      <SCRIPT>
+								        var demo6 = document.getElementById('demo6');
+										var demo7 = document.getElementById('demo7');
+										var demo8 = document.getElementById('demo8');  
 								      	$(document).ready(function(){
 											var speed=30//速度数值越大速度越慢
+											
 											demo8.innerHTML=demo7.innerHTML
 											function Marquee(){
 											if(demo8.offsetWidth-demo6.scrollLeft<=0)
--- show/show.post.php
+++ show/show.post.php
@@ -15,8 +15,6 @@
 				redirect($_SERVER['HTTP_REFERER']);
 			}
 			$image->src = "/upload/images/" .$img;
-			$image->create_thumb('middle',50);
-			$image->create_thumb('small',170,70);
 		}
 		$image->category_id=category_id_by_name('我要上传','picture');
 		$image->commentable = 'on';
@@ -53,6 +51,6 @@
 		$video->created_at = date("Y-m-d H:i:s");
 		$video->save();
 	}
-	alert("上传成功！谢谢！");
+	alert("上传成功！请等待管理员审批！");
 	redirect($_SERVER['HTTP_REFERER']);
 ?>
