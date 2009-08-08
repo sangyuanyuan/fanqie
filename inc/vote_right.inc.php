2c8c0d1245cdcfb634a9b2487c5ec76cbae608bb
sauger <sauger@163.com>
2009-08-08 15:27:25 +0800

    fix bugs

--- inc/vote_right.inc.php
+++ inc/vote_right.inc.php
@@ -1,6 +1,6 @@
 <?php
 	$db = get_db();
-	$latest_votes = $db->query("select * from smg_vote where category_id>0 and is_adopt=1 order by id desc limit 10");
+	$latest_votes = $db->query("select * from smg_vote where is_sub_vote = 0 and  is_adopt=1 order by id desc limit 10");
 ?>
 <div id=right>
 			<div id=title><img src="/images/pic/answer_right.gif">热门投票</div>
