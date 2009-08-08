28a795786969444306f6b926d25c7a9fa4723ef9
robbin17 <robbin_szf@hotmail.com>
2009-08-08 13:35:21 +0800

    robbin

--- subject/sunmoon/index.php
+++ subject/sunmoon/index.php
@@ -1,4 +1,5 @@
-﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
+﻿<?php require_once('../../frame.php'); ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
 <HTML><HEAD><TITLE>日全食专题</TITLE>
 	<meta http-equiv=Content-Type content="text/html; charset=utf-8">
 <META content="" name=keywords>
