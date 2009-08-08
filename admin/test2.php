cd7358529da8deb4b4489ae545cfcc80f286c8c2
justin1986 <zhanghao@xun-ao.com>
2009-08-08 10:25:29 +0800

    test

--- admin/test2.php
+++ admin/test2.php
@@ -3,9 +3,9 @@
 	$db = get_db();
 	$sql = 'select dept_id,dept_category_id,title,created_at from smg_new_debug where dept_category_id!=0';
 	$record = $db->query($sql);
-	for($i=0;$i<$record;$i++){
+	$count = count($record);
+	for($i=0;$i<$count;$i++){
 		$sql = 'update smg_news set dept_category_id='.$record[$i]->dept_category_id.' where dept_id='.$record[$i]->dept_id.' and title="'.$record[$i]->title.'" and created_at='.$record[$i]->created_at;
-		echo $sql;
 		$db->execute($sql);
 	}
 ?>
