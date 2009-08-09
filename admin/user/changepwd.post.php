8093a8c9a1ff401b442a113e6f1da4774e0a7e80
sauger <sauger@163.com>
2009-08-09 12:27:27 +0800

    add user admin

---
+++ admin/user/changepwd.post.php
@@ -0,0 +1,9 @@
+<?php
+	include "../../frame.php";
+	$db = get_db();
+	if($db->execute("update smg_user set password = '{$_POST['pwd']}' where id={$_POST['id']}")){
+		echo '修改密码成功!';
+	}else{
+		echo '修改密码失败:' .$db->last_error;
+	}
+?>
