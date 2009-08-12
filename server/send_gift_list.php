999ac180b9b96cab937f06ab756909ec124879e5
sauger <sauger@163.com>
2009-08-12 16:38:52 +0800

    add send back function in birthday gifts

--- server/send_gift_list.php
+++ server/send_gift_list.php
@@ -33,21 +33,22 @@
   
         <div id="context">
 			<?php
-				$sql = "select sender,reciever,created_at,count(*) as icount ,b.nick_name from smg_birthday_gift a left join smg_user b on a.reciever = b.name group by sender,reciever,created_at order by a.id desc";
+				$sql = "select sender,reciever,created_at,send_type,count(*) as icount ,b.nick_name from smg_birthday_gift a left join smg_user b on a.reciever = b.name group by sender,reciever,created_at, send_type order by a.id desc";
 				$records = $db->paginate($sql,9);
 				$count = count($records);
 				for($i=0;$i<$count;$i++){
 					$nick_name = $records[$i]->nick_name ? $records[$i]->nick_name : $records[$i]->reciever;
 					$sender = urlencode($records[$i]->sender);
 					$reciever = urlencode($records[$i]->reciever);					
-					$url = "send_gift_list_detail.php?sender={$sender}&reciever={$reciever}&send_date={$records[$i]->created_at}"
+					$url = "send_gift_list_detail.php?sender={$sender}&reciever={$reciever}&send_date={$records[$i]->created_at}&send_type={$records[$i]->send_type}";
+					$send_type_string = $records[$i]->send_type == 1 ? '回赠' : '赠送';
 			?>
 			<div class=box style="float:left;display:inline">
 				<div class=gift  style="float:left;display:inline"></div>
 				<a>
 				<div class=info  style="float:left;display:inline">
 					<div class=giver><?php echo $records[$i]->sender; ?></div>
-					赠送 <b><?php echo $nick_name;?></b>&nbsp;<font color=#FF0000 style="font-weight:bolder;">生日礼物</font>
+					<?php echo $send_type_string ;?> <b><?php echo $nick_name;?></b>&nbsp;<font color=#FF0000 style="font-weight:bolder;">生日礼物</font>
 				</div>
 				
 				<div class=info  style="float:left;display:inline"><?php echo $records[$i]->icount;?> 份</div>
