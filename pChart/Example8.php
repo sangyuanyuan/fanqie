57dbdb15f7b149284d77b0f5867b4b6ad05d6f97
robbin17 <robbin_szf@hotmail.com>
2009-08-26 13:37:53 +0800

    robbin

--- pChart/Example8.php
+++ pChart/Example8.php
@@ -19,6 +19,7 @@
 
  // Initialise the graph
  $Test = new pChart(400,400);
+ $Test->reportWarnings("GD");
  $Test->setFontProperties("Fonts/zhunyuan.ttf",8);
  $Test->drawFilledRoundedRectangle(7,7,393,393,5,240,240,240);
  $Test->drawRoundedRectangle(5,5,395,395,5,230,230,230);
