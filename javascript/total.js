e50a95ec589fb9e2bc56563121302a54327f8faa
robbin17 <robbin_szf@hotmail.com>
2009-09-10 16:28:45 +0800

    robbin

--- javascript/total.js
+++ javascript/total.js
@@ -6,7 +6,7 @@ var PostStr="";
 if(window.XMLHttpRequest){showhttp = new XMLHttpRequest();if (showhttp.overrideMimeType) {showhttp.overrideMimeType('text/xml');}}	else if (window.ActiveXObject){try {showhttp = new ActiveXObject("Msxml2.XMLHTTP");} catch (e) {try {showhttp = new ActiveXObject("Microsoft.XMLHTTP");} catch (e) {}}}
 function Post(url,section,mvalue){	var mdata;		if (!showhttp) { window.alert("不能创建XMLHttpRequest对象实例.");return false;	}		showhttp.open("POST", url, true);			showhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded");		mdata = section+"="+mvalue;	showhttp.send(mdata);}
 
-function total(param1,param2){alert(param1);alert(param2); PostStr=param1+PostDiv+param2;Post(Urls,"total",PostStr,rtotal);}
+function total(param1,param2){PostStr=param1+PostDiv+param2;Post(Urls,"total",PostStr,rtotal);}
 function rtotal()
 {
     if (showhttp.readyState == 4) { 
