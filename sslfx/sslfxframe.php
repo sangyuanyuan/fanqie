defa6ef15d1ff126b003c9ca3c027e68666503b2
robbin17 <robbin_szf@hotmail.com>
2009-09-11 14:00:27 +0800

    robbin

--- sslfx/sslfxframe.php
+++ sslfx/sslfxframe.php
@@ -6,8 +6,8 @@
 	<meta http-equiv=Content-Language content=zh-cn>
 	<style>
 		html { overflow-x:hidden; }
-		table{width:550px; float:left; display:inline;}
-		tr{width:550px; float:left; display:inline;}
+		table{width:590px; float:left; display:inline;}
+		tr{width:590px; float:left; display:inline;}
 		td{width:12.5%;}
 	</style>
 </head>
