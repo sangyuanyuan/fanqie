e72d738bf216a49f0552e299028b2b72eef4b1a5
robbin17 <robbin_szf@hotmail.com>
2009-09-21 14:41:08 +0800

    robbin

--- server/friend_list.php
+++ server/friend_list.php
@@ -38,9 +38,9 @@
     		<div id="title"><div id=date>TODAY <?php echo $date;?></div></div>
      	  <div id="menu">
      	  	<div id="menu2"><a href="birthday.php">我的生日</a></div>
-     	  	<div id="menu2"><a href="friend_list.php">我的好友</a></div>
+     	  	<div id="menu1"><a href="friend_list.php">我的好友</a></div>
     	  	<div id="menu2"><a href="calendar.php">日历</a></div>
-     	  	<div id="menu1">今日寿星</div>	
+     	  	<div id="menu2">今日寿星</div>	
 			<div id="menu2"><a href="send_gift_list.php">送礼列表</a></div>
      	  	<div id="menu2"><a href="birthday_top.php">寿星排行</a></div>
 			<div id="menu2"><a href="birthday_search.php">寿星查询</a></div>
