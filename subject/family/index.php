9353669c792101e711bbb1c75b91f616868a7390
robbin17 <robbin_szf@hotmail.com>
2009-10-09 17:59:04 +0800

    robbin

---
+++ subject/family/index.php
@@ -0,0 +1,20 @@
+﻿<?php
+	require_once('../../frame.php');
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+	<meta http-equiv=Content-Language content=zh-cn>
+	<title>SMG-番茄网-专题-全家都来赛</title>
+	<LINK href="css/index.css" type=text/css rel=stylesheet>
+	<?php 
+		
+	?>
+</head>
+<body>
+	<div id="fbody">
+		<div id="bmrs">88888</div>
+	</div>
+</body>
+</html>
