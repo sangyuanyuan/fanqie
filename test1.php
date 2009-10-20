8c53515199a36dc83251873942122b030b082a7b
robbin17 <robbin_szf@hotmail.com>
2009-10-20 14:43:08 +0800

    robbin

--- test1.php
+++ test1.php
@@ -1,7 +1,13 @@
-﻿
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+	<meta http-equiv=Content-Language content=zh-cn>
+	<title>SMG-番茄网-首页</title>
+</head>
 <body>
 	
-<OBJECT   id=MediaPlayer1   codeBase= http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=5,1,52,701standby=Loading   type=application/x-oleobject   height=265   width=285   classid=CLSID:6BF52A52-394A-11d3-B153-00C04F79FAA6   VIEWASTEXT> 
+			<OBJECT   id=MediaPlayer1   codeBase= http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=5,1,52,701standby=Loading   type=application/x-oleobject   height=265   width=285   classid=CLSID:6BF52A52-394A-11d3-B153-00C04F79FAA6   VIEWASTEXT> 
 				<PARAM   NAME= "URL"   VALUE= "mms://172.27.202.23/itvod/tech0905.wmv"> 
 				<PARAM   NAME= "windowlessVideo"   VALUE="0">
 				<PARAM   NAME= "playCount"   VALUE= "1"> 
@@ -11,5 +17,6 @@
 				<PARAM   NAME= "EnableContextMenu"   VALUE= "true">			
 				<embed src="/upload ideo/QsUYdq0Izl.wmv" align="baseline" border="0" width="285" height="265" type="application/x-mplayer2"pluginspage="" name="MediaPlayer1" showcontrols="1" showpositioncontrols="0" showaudiocontrols="1" showtracker="1" showdisplay="0" showstatusbar="1" autosize="0" showgotobar="0" showcaptioning="0" autostart="false" autorewind="0" animationatstart="0" transparentatstart="0" allowscan="1" enablecontextmenu="1" clicktoplay="0" defaultframe="datawindow" invokeurls="0"></embed> 
 			</OBJECT> 		
-	
+		<?php echo urlencode(iconv('utf-8','gbk','第198期（一版·要闻）')); ?>
 </body>
+
