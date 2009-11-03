873f59a320b67a07a1ae972fd6325938dd4d6ef2
robbin17 <robbin_szf@hotmail.com>
2009-11-03 11:43:16 +0800

    robbin

--- admin/updatepwd/resetpwd.php
+++ admin/updatepwd/resetpwd.php
@@ -11,6 +11,10 @@
 	js_include_tag("total");
 	validate_form('login');
 	$cookie= (isset($_COOKIE['smg_admin'])) ? $_COOKIE['smg_admin'] : 0;
+	if($cookie==0)
+	{
+			redirect('resetpwdlogin.php','js');
+	}
 ?>
 <script>
 	total("重置密码","other");	
