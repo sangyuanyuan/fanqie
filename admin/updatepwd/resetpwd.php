3e315f80351ee23eeb88814b54397a3eead67015
robbin17 <robbin_szf@hotmail.com>
2009-11-03 11:24:59 +0800

    robbin

--- admin/updatepwd/resetpwd.php
+++ admin/updatepwd/resetpwd.php
@@ -3,22 +3,23 @@
 <head>
 	<meta http-equiv=Content-Type content="text/html; charset=utf-8">
 	<meta http-equiv=Content-Language content=zh-CN>
+	<link href="/css/login.css" rel="stylesheet" type="text/css">
 	<title>SMG</title>
 </head>
 <? 
 	require_once("../../frame.php");
-	css_include_tag('login');
 	js_include_tag("total");
 	validate_form('login');
-	$cookie= (isset($_COOKIE['smg_admin'])) ? $_COOKIE['smg_admin'] : 0;?>
+	$cookie= (isset($_COOKIE['smg_admin'])) ? $_COOKIE['smg_admin'] : 0;
+?>
 <script>
 	total("重置密码","other");	
 </script>
 <body>
-<div id=login>
-		<div id=login  style="background:none">
-			<div id=title>SMG重置密码    </div>
-			<form name=login id=login method="post" action="updatepwd.post.php">
+<div id=main>
+	<div id=login>
+		<div id=title>SMG重置密码    </div>
+		<form name=login id=login method="post" action="updatepwd.post.php">
 			<div id=box style="border:1px solid #0066FF; background:#CBEBFA">
 				<div id=name style="margin-top:15px;" >工　　号：　<input type="text" name="updatepwd[userid]" style="width:140px; height:17px;" class="required"></div>
 				<div id=pwd >联系方式：　<input style="width:140px; height:17px;" type="text" name="updatepwd[phone]" class="required"></div>
@@ -26,9 +27,9 @@
 				<input type="hidden" name="updatepwd[subtype]" value="resetpwd">
 				<div id=zhu>“番茄网”重置密码指南：<br>· 输入需要修改员工工号×××××××××。<br>· 输入联系方式</div>	
 			</div>
-			</form>	
-		</div>		
-	</div>	
+		</form>	
+	</div>
+</div>
 </body>
 </form>
 </html>
