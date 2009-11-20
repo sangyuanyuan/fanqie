ac6a8b2152df0aa5053173ca23b6bb45580d1415
robbin17 <robbin_szf@hotmail.com>
2009-11-20 17:36:51 +0800

    robbin

--- admin/admin.php
+++ admin/admin.php
@@ -11,7 +11,7 @@
 <head>
 	<meta http-equiv=Content-Type content="text/html; charset=utf-8">
 	<meta http-equiv=Content-Language content=zh-CN>
-	<title>yuxiao</title>
+	<title>smg</title>
 	<?php	
 		css_include_tag('admin');
 		use_jquery();
