4b34faecb9e082a32582201870ef00da0e09c3f0
robbin17 <robbin_szf@hotmail.com>
2009-12-01 15:28:39 +0800

    robbin

--- admin/tg/fhtg_add.php
+++ admin/tg/fhtg_add.php
@@ -60,7 +60,7 @@
 		?>
 		<input type="hidden" name="item_num" id="num" value="2">
 		<tr class="tr3">
-			<td colspan="2" width="795" align="center"><button type="button" id="sub">发布复合团购</button></td>
+			<td colspan="2" width="795" align="center"><button type="submit" id="sub">发布复合团购</button></td>
 		</tr>
 		<input type="hidden" name="fhtg[is_adopt]" value="0">
 		<input type="hidden" name="fhtg[created_at]" value="<?php echo date("Y-m-d");?>">
@@ -83,9 +83,6 @@
 				num--;
 				$("#num").attr('value',num);
 			});
-		});
-		$("#sub").click(function(){
-			$("#fhtg_add").submit();
 		});
 	});
 	$(".date_jquery").datepicker(
