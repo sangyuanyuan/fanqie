d4c24d1f629c11ce6c77cec181ee4448ad4bf6d4
robbin17 <robbin_szf@hotmail.com>
2009-12-02 16:46:25 +0800

    robbin

---
+++ zp.php
@@ -0,0 +1,8 @@
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+</head>
+<body style="width:995px; margin:0 auto; background:black;">
+<embed src="zp.swf" quality=high pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="995" height="650"></embed>
+</body>
+</html>
