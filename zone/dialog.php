2202d64d2d6927f8b225ef1218e38b10991a8d70
robbin17 <robbin_szf@hotmail.com>
2009-12-03 13:31:14 +0800

    robbin

--- zone/dialog.php
+++ zone/dialog.php
@@ -101,11 +101,11 @@
 					<div id="div_q_emotion" style="width:100%;float:left;"></div>
 				</div>
 				<?php 
-					/*if($dialog->video_url)
-					{*/
+					if($dialog->video_url)
+					{
 				?>		
 						<div id=video>
-							<OBJECT   id=MediaPlayer1   codeBase=http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=5,1,52,701standby=Loading   type=application/x-oleobject   height=414   width=537   classid=CLSID:6BF52A52-394A-11d3-B153-00C04F79FAA6   VIEWASTEXT> 
+							<!--<OBJECT   id=MediaPlayer1   codeBase=http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=5,1,52,701standby=Loading   type=application/x-oleobject   height=414   width=537   classid=CLSID:6BF52A52-394A-11d3-B153-00C04F79FAA6   VIEWASTEXT> 
 								<PARAM   NAME= "URL"   VALUE= "mms://172.27.202.23:5765/broadcast"> 
 								<PARAM   NAME= "playCount"   VALUE= "1"> 
 								<PARAM   NAME= "autoStart"   VALUE= "true"> 
@@ -113,13 +113,13 @@
 								<PARAM   NAME= "uiMode"   VALUE= "Full">
 								<PARAM   NAME= "EnableContextMenu"   VALUE= "true">			
 								<embed src="mms://172.27.202.23:5765/broadcast" align="baseline" border="0" width="537" height="414" type="application/x-mplayer2"pluginspage="" name="MediaPlayer1" showcontrols="1" showpositioncontrols="0" showaudiocontrols="1" showtracker="1" showdisplay="0" showstatusbar="1" autosize="0" showgotobar="0" showcaptioning="0" autostart="false" autorewind="0" animationatstart="0" transparentatstart="0" allowscan="1" enablecontextmenu="1" clicktoplay="0" defaultframe="datawindow" invokeurls="0"></embed> 
-							</OBJECT>
-							<?php //show_video_player('537','414',$dialog->photo_url,$dialog->video_url,$autostart = "false");
+							</OBJECT>-->
+							<?php show_video_player('537','414',$dialog->photo_url,$dialog->video_url,$autostart = "false");
 							?>		
 						</div>
 					
 				<?
-					//}
+					}
 				?>
 				<div id="answer_title"></div>
 
