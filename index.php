61558e4d10d3395bdbc32f07cf3d689d17c7cb57
robbin17 <robbin_szf@hotmail.com>
2009-12-07 10:10:20 +0800

    robbin

--- index.php
+++ index.php
@@ -976,11 +976,21 @@ total("首页","other");
  
       <!-- start bottom_top_right !-->
  			<div id=b_t_r>
+ 				<div class=l_box>
+ 					<?php $sql = 'select id,title from smg_question where is_adopt=1 order by create_time desc limit 6';
+ 								$record = $db->query($sql);
+ 					 ?>
+ 					<div class="top_title"><a href="/xlcs/xlcs.php?xlcsid=<?php echo $record[0]->id;?>"  target=_blank>答题：<?php echo $record[0]->title;?></a></div>
+ 					<?php for($i=1;$i<count($record);$i++)
+						{?>
+						<div class="bottom_title"><li><span style="color:#FF9900">·</span><a href="/xlcs/xlcs.php?xlcsid=<?php echo $record[$i]->id;?>"  target=_blank>答题：<?php echo $record[$i]->title;?></a></li></div>
+					 <?php } ?>
+ 				</div>
 				<div class=m_box>
 					<?php  
-						$sql = 'select id,title from smg_question where is_adopt=1 order by create_time desc limit 4';		
+								
 						//$sql = 'select id,name from smg_problem where is_adopt=1 order by create_time desc limit 6';
-						$record = $db->query($sql);
+						
 						$sql="select id,title from smg_xlcs where is_adopt=1 order by created_at desc limit 6";
 						$xlcs=$db->query($sql);
 						$count = count($record);
