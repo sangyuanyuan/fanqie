04d854e4821c7bff29c09eb2dcaa357ce8472a8e
robbin17 <robbin_szf@hotmail.com>
2009-12-15 16:09:03 +0800

    robbin

--- lib/pubfun.php
+++ lib/pubfun.php
@@ -445,7 +445,7 @@ function search_content($key,$table_name='smg_news',$conditions=null,$page_count
 	if($conditions){
 		$d = $conditions . ' and (' .$d .')';
 	}
-	$sql1 = 'select a.id,a.short_title,a.title,a.dept_id,a.category_id,a.created_at from (select * from ' . $table_name ." where 1=1 and ".$d;
+	$sql1 = 'select a.id,a.short_title,a.title,a.dept_id,a.category_id,a.created_at,a.is_adopt from (select * from ' . $table_name ." where 1=1 and ".$d;
 	if($order)
 	{
 		$sql1 = $sql1 . ' order  by ' .$order;
@@ -464,11 +464,11 @@ function search_content($key,$table_name='smg_news',$conditions=null,$page_count
 	}
 	if($e!="")
 	{
-		$sql2 = 'select b.id,b.title,b.short_title,b.dept_id,b.category_id,b.created_at from (select * from ' . $table_name .' where 1=1 and id not in ('.$e.') and ('.$c.')';
+		$sql2 = 'select b.id,b.title,b.short_title,b.dept_id,b.category_id,b.created_at,b.is_adopt from (select * from ' . $table_name .' where 1=1 and id not in ('.$e.') and ('.$c.')';
 	}
 	else
 	{
-		$sql2 = 'select b.id,b.title,b.short_title,b.dept_id,b.category_id,b.created_at from (select * from ' . $table_name ." where 1=1 and ".$c;	
+		$sql2 = 'select b.id,b.title,b.short_title,b.dept_id,b.category_id,b.created_at,b.is_adopt from (select * from ' . $table_name ." where 1=1 and ".$c;	
 	}
 	if($order)
 	{
