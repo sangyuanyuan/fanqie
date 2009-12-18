6dc44689076751b73c6ff54a6a5e094b81ea81d0
robbin17 <robbin_szf@hotmail.com>
2009-12-18 16:47:53 +0800

    robbin

--- subject/xyy2/index.php
+++ subject/xyy2/index.php
@@ -116,6 +116,8 @@ js_include_once_tag('total','gd');?>
 						picflash.addVariable("borderheight","180");
 						picflash.addVariable("borderw","false");
 						picflash.addVariable("buttondisplay","false");
+						picflash.addVariable("textheight","20");
+						picflash.addVariable("textcolor","#FF0000");
 						picflash.addVariable("pic_width",pic_width);
 						picflash.addVariable("pic_height",pic_height);
 						
