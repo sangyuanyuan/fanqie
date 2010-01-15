6ba3b7823c080d37d4f298377c8bb854a8405f80
robbin17 <robbin_szf@hotmail.com>
2010-01-15 17:43:43 +0800

    robbin

--- inc/top.inc.php
+++ inc/top.inc.php
@@ -32,7 +32,7 @@
 </div>
 <div id=nimg1>
 	<?php $days=daysInSpan(mktime(0,0,0,date('m'),date('d'),date('Y')),mktime(0,0,0,11,01,2009));?>
-	<!--<img border=0 src="/images/yuan.jpg">-->
+	<a target="_blank" href="/subject/jiang/"><img border=0 src="/images/jiang.jpg"></a>
 </div>
 <div id=nav2>
 	
