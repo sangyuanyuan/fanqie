4e97f239b9ac80f50f0b9c222acc0ee3b8ddc019
robbin17 <robbin_szf@hotmail.com>
2010-01-21 12:47:35 +0800

    robbin

--- index_video.php
+++ index_video.php
@@ -12,7 +12,7 @@
 </head>
 
 <body>
-<!--<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="235" height="182" id="FLVPlayer">
+<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="235" height="182" id="FLVPlayer">
  <param name="movie" value="/flash/mediaplayer.swf" />
  <param name="salign" value="lt" />
  <param name="quality" value="high" />
@@ -24,7 +24,7 @@
  <embed src="/flash/mediaplayer.swf" flashvars="&image=<?php echo $_REQUEST['photo']?>&file=<?php echo $_REQUEST['video'] ?>&displayheight=167&autostart=false" quality="high" scale="noscale" width="235" height="182" name="FLVPlayer" wmode="opaque" salign="LT" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
 </object>
 
-<OBJECT   id=MediaPlayer1   codeBase=http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=5,1,52,701standby=Loading   type=application/x-oleobject   height=245   width=235   classid=CLSID:6BF52A52-394A-11d3-B153-00C04F79FAA6   VIEWASTEXT> 
+<!--<OBJECT   id=MediaPlayer1   codeBase=http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=5,1,52,701standby=Loading   type=application/x-oleobject   height=245   width=235   classid=CLSID:6BF52A52-394A-11d3-B153-00C04F79FAA6   VIEWASTEXT> 
 				<PARAM   NAME= "URL"   VALUE= "mms://172.27.202.23:5765/broadcast"> 
 				<PARAM   NAME= "playCount"   VALUE= "1"> 
 				<PARAM   NAME= "autoStart"   VALUE= "true"> 
@@ -33,14 +33,5 @@
 				<PARAM   NAME= "EnableContextMenu"   VALUE= "true">			
 				<embed src="mms://172.27.202.23:5765/broadcast" align="baseline" border="0" width="288" height="230" type="application/x-mplayer2"pluginspage="" name="MediaPlayer1" showcontrols="1" showpositioncontrols="0" showaudiocontrols="1" showtracker="1" showdisplay="0" showstatusbar="1" autosize="0" showgotobar="0" showcaptioning="0" autostart="false" autorewind="0" animationatstart="0" transparentatstart="0" allowscan="1" enablecontextmenu="1" clicktoplay="0" defaultframe="datawindow" invokeurls="0"></embed> 
 			</OBJECT>-->
-<OBJECT   id=MediaPlayer1   codeBase=http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=5,1,52,701standby=Loading   type=application/x-oleobject   height=245   width=235   classid=CLSID:6BF52A52-394A-11d3-B153-00C04F79FAA6   VIEWASTEXT> 
-				<PARAM   NAME= "URL"   VALUE= "mms:\\172.27.202.23\ttv"> 
-				<PARAM   NAME= "playCount"   VALUE= "1"> 
-				<PARAM   NAME= "autoStart"   VALUE= "true"> 
-				<PARAM   NAME= "invokeURLs"   VALUE= "false">
-				<PARAM   NAME= "uiMode"   VALUE= "Full">
-				<PARAM   NAME= "EnableContextMenu"   VALUE= "true">			
-				<embed src="mms://172.27.202.23:5765/broadcast" align="baseline" border="0" width="288" height="230" type="application/x-mplayer2"pluginspage="" name="MediaPlayer1" showcontrols="1" showpositioncontrols="0" showaudiocontrols="1" showtracker="1" showdisplay="0" showstatusbar="1" autosize="0" showgotobar="0" showcaptioning="0" autostart="false" autorewind="0" animationatstart="0" transparentatstart="0" allowscan="1" enablecontextmenu="1" clicktoplay="0" defaultframe="datawindow" invokeurls="0"></embed> 
-			</OBJECT>
 </body>
 </html>
