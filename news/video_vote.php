29d83248d7cd50df6e172b30bb3438558a2ea555
robbin17 <robbin_szf@hotmail.com>
2010-01-25 14:59:25 +0800

    robbin

--- news/video_vote.php
+++ news/video_vote.php
@@ -28,14 +28,14 @@
 		<div id=left>
 			<div style="width:980px; font-size:20px; font-weight:bold; color:red; text-align:center; margin-top:10px; margin-left:15px; line-height:22px; float:left; display:inline;"><?php echo delhtml($vote->name);?></div>
 <div style="width:980px; font-size:14px; font-weight:bold; color:red; margin-top:10px; margin-left:15px; line-height:18px; float:left; display:inline;">
-	@_@“大眼番茄09最佳视频新闻奖”评选活动开始了！ 在过去的一年中，各部门发来了很多制作精良、有吸引力的视频新闻，视频新闻相比文字新闻制作更加耗时耗力，但也往往更受网友欢迎，在2009年，网友筒子们有哪些印象深刻的视频新闻？<br>
-   番茄网特别举行“大眼番茄09最佳视频新闻奖”评选活动，请大家不要吝啬点击手中的小鼠标，为心目中的“09最佳视频新闻”投上一票吧！(点击图片可看原文和视频)<br>
+　　@_@“大眼番茄09最佳视频新闻奖”评选活动开始了！ 在过去的一年中，各部门发来了很多制作精良、有吸引力的视频新闻，视频新闻相比文字新闻制作更加耗时耗力，但也往往更受网友欢迎，在2009年，网友筒子们有哪些印象深刻的视频新闻？<br>
+　　番茄网特别举行“大眼番茄09最佳视频新闻奖”评选活动，请大家不要吝啬点击手中的小鼠标，为心目中的“09最佳视频新闻”投上一票吧！(点击图片可看原文和视频)<br>
 活动办法：<br>
-    1、番茄小编根据2009年发布在“番茄网”上视频新闻的点击量，整理后初选出15则视频新闻入围。<br>
-    2、即日起至2月7日为网友网络匿名投票时间。<br>
-    3、春节前公布最终评选结果。<br>
+　　1、番茄小编根据2009年发布在“番茄网”上视频新闻的点击量，整理后初选出15则视频新闻入围。<br>
+　　2、即日起至2月7日为网友网络匿名投票时间。<br>
+　　3、春节前公布最终评选结果。<br>
 奖项设置：<br>
- “大眼番茄09最佳视频新闻奖”3个，获奖者有特别大礼相赠噢！！</div>
+ 　“大眼番茄09最佳视频新闻奖”3个，获奖者有特别大礼相赠噢！！</div>
 			<div id="vote_container_box" style="width:100%; overflow:hidden; float:left;text-align:center">
 				<div id=pic><?php if($vote->photo_url!=''){?><img border=0 src="<?php echo $vote->photo_url; ?>"><?php } ?></div>
 				<?php $vote->display(array('show_title' => false,'target'=>'_blank'));?>
