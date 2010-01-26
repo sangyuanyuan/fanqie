6c09f92ae7b66a48881e57ce4d35e92508c0d454
robbin17 <robbin_szf@hotmail.com>
2010-01-26 09:49:16 +0800

    robbin

--- learn/learning.php
+++ learn/learning.php
@@ -50,7 +50,7 @@
 			  	$pics = $db->query('select i.src from smg_images i inner join smg_category c on i.category_id=c.id and i.is_adopt = 1 and c.name="重磅推荐" and c.category_type="picture" order by i.priority asc, i.created_at desc limit 1');
 			  	$pic = $pics[0]->src;
 			  }
-			  
+			  $zbtj = $db->query('select s.id,s.is_photo_news,s.photo_src,s.click_count,s.title,s.description,c.platform,c.id as cid from smg_news s inner join smg_category c on s.category_id=c.id and is_adopt = 1 and c.name ="重磅推荐" and c.category_type="news" order by s.priority asc,s.created_at desc limit 1');
 			  ?>
 				<a target="_blank" href="/<?php echo $zbtj[0]->platform;?>/news/news.php?id=<?php echo $zbtj[0]->id;?>"><?php echo $zbtj[0]->title;?></a>
 				<div id=left><img width=90 height=94 src="<?php echo $pic;?>" /><a target="_blank" href="/<?php echo $zbtj[0]->platform; ?>/news/news.php?id=<?php echo $zbtj[0]->id;?>">[点击查询详情]</a></div>
