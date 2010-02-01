c51b7774a56d3b3acf5edad2406e40e6d119d5ab
robbin17 <robbin_szf@hotmail.com>
2010-02-01 09:23:47 +0800

    robbin

--- inc/top.inc.php
+++ inc/top.inc.php
@@ -32,7 +32,7 @@
 </div>
 <div id=nimg1>
 	<?php $days=daysInSpan(mktime(0,0,0,date('m'),date('d'),date('Y')),mktime(0,0,0,11,01,2009));?>
-	<a target="_blank" href="/show/video.php?id=1322"><img border=0 src="/images/top.gif"></a>
+	<!--<a target="_blank" href="/show/video.php?id=1322"><img border=0 src="/images/top.gif"></a>-->
 </div>
 <div id=nav2>
 	
