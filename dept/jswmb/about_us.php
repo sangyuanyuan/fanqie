9ecf428a9be976bfcc54829cfe3690557faabf54
robbin17 <robbin_szf@hotmail.com>
2010-02-04 10:35:15 +0800

    robbin

--- dept/jswmb/about_us.php
+++ dept/jswmb/about_us.php
@@ -31,9 +31,9 @@
 			<div class="news_box" style="font-size:15px;">
 			<br>
 				关于我们：<br>
-&nbsp;&nbsp;&nbsp;&nbsp;集团精神文明建设领导小组负责集团的精神文明创建工作。领导小组成员由集团党委委员组成。集团精神文明建设领导小组下设办公室，和集团办公室一起，作为精神文明建设领导小组的日常工作机构。<br><br>
-&nbsp;&nbsp;&nbsp;&nbsp;集团文明办主任：黄秋源 <br><br>
-&nbsp;&nbsp;&nbsp;&nbsp;成员：张耀春 朱顺康 李建军 周萍<br><br>
+&nbsp;&nbsp;&nbsp;&nbsp;台、集团精神文明建设领导小组负责台、集团的精神文明创建工作。领导小组成员由台、集团党委委员组成。台、集团精神文明建设领导小组下设办公室，和台、集团办公室一起，作为精神文明建设领导小组的日常工作机构。<br><br>
+&nbsp;&nbsp;&nbsp;&nbsp;台、集团文明办主任：黄秋源 <br><br>
+&nbsp;&nbsp;&nbsp;&nbsp;成员：张耀春 朱顺康 李建军 张天恩<br><br>
 &nbsp;&nbsp;&nbsp;&nbsp;电话：62565899转
 
 			</div>
