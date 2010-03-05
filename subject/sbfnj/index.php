e8bde98ac61927c7108e5939595e3ff48e3bd2d8
robbin17 <robbin_szf@hotmail.com>
2010-03-05 14:59:08 +0800

    robbin

--- subject/sbfnj/index.php
+++ subject/sbfnj/index.php
@@ -69,9 +69,9 @@ ul,li{margin:0px; padding:0px;list-style:none;}
 </table>
 <table  cellSpacing=0 cellPadding=0 width=770 align=center border=0>
   <tr>
-  	<td id="mail_to_mcdull" width=215>
+  	<td valign=top id="mail_to_mcdull" width=215>
   		<div id=title1>三八妇女节祝福</div>
-  		<div style="width:204px; height:305px; margin-left:10px; margin-top:5px; background:url(css/tomcdull.jpg);  float:left; display:inline;">
+  		<div style="width:204px; height:305px; margin-left:10px; margin-top:5px; float:left; display:inline;">
 			<? $newslist=$db->query('select * from smg_comment where resource_type="sbfnj" order by created_at desc');?>
 	  		<marquee height="300" DIRECTION="up" scrollamount="2" onmouseover=this.stop() onmouseout=this.start()>
 					<? for($i=0; $i<count($newslist); $i++){?>
