f0cb89baa08cb59325181abb9f497fc7c2a1f821
robbin17 <robbin_szf@hotmail.com>
2010-03-25 17:19:19 +0800

    robbbin

--- subject/sxxx2/index.php
+++ subject/sxxx2/index.php
@@ -64,9 +64,9 @@
 		        $thumb=$doc->createElement("thumb_image");   
 		        $thumb=$info->appendChild($thumb); 
 		          
-		        $name->appendChild($doc->createTextNode(''.mb_substr(strip_tags($pic[0]->short_title),0,9,"utf-8").''));  #createTextNode创建内容的子节点，然后把内容添加到节点中来      
+		        $name->appendChild($doc->createTextNode(mb_substr(strip_tags($pic[0]->short_title),0,9,"utf-8")));  #createTextNode创建内容的子节点，然后把内容添加到节点中来      
 		        $sex->appendChild($doc->createTextNode("/news/news/news.php?id=".$pic[$i]->id));
-		        $thumb->appendChild($doc->createTextNode('../..'.$pic[$i]->photo_src)); 
+		        $thumb->appendChild($doc->createTextNode($pic[$i]->photo_src)); 
 	  }      
 	  $doc->save("imglink.xml"); 
 	?>
