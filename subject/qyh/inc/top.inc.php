a546881f3ff050ef678238b30f793f5af2565a00
robbin17 <robbin_szf@hotmail.com>
2010-03-29 16:06:31 +0800

    robbin

--- subject/qyh/inc/top.inc.php
+++ subject/qyh/inc/top.inc.php
@@ -2,7 +2,7 @@
 	<div id=logo></div>
 	<div id=top_title>
 		<div id=dh>
-			<a target="_blank" href="">首 页</a><a target="_blank" href="">群英闪烁</a><a target="_blank" href="">群英们的故事</a><a target="_blank" href="">群英们的足迹</a><a target="_blank" href="">奖项介绍</a><a target="_blank" href="">相关视频</a><a target="_blank" href="">心灵之光</a><a target="_blank" href="">对话群英</a>
+			<a target="_blank" href="">首 页</a><a target="_blank" href="/news/news_list.php?id=206">群英闪烁</a><a target="_blank" href="/news/news_list.php?id=200">群英们的故事</a><a target="_blank" href="/news/news_list.php?id=201">群英们的足迹</a><a target="_blank" href="/news/news_list.php?id=202">奖项介绍</a><a target="_blank" href="/news/news_list.php?id=196">心灵之光</a><a target="_blank" href="dialog.php">对话群英</a>
 		</div>		
 	</div>
 </div>
