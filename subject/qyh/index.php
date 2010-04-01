af4ee13ca076534f30f0cbc77a223545d9c8dad4
robbin17 <robbin_szf@hotmail.com>
2010-04-01 14:26:30 +0800

    robbin

--- subject/qyh/index.php
+++ subject/qyh/index.php
@@ -176,10 +176,10 @@
 				
 				<div class=index_b_r>
 					<div class=title>群英博客</div>
-					<?php  $news=$db->query('select photo_src,id,description,title from smg_news where category_id=203 and is_adopt=1 order by priority asc,created_at desc limit 12');?>
+					<?php  $news=$db->query('select photo_src,id,description,title from smg_news where category_id=203 and is_adopt=1 order by priority asc,created_at desc limit 4');?>
 					<div class=content>
 						<?php for($i=0;$i<count($news);$i++){ ?>
-						<div class=pic><a target="_blank" href="/show/news/news.php?id=<?php echo $news[$i]->id; ?>"><img src="<?php echo $news[$i]->photo_src; ?>"></a></div>
+						<div class=pic><a target="_blank" href="/show/news/news.php?id=<?php echo $news[$i]->id; ?>"><img border=0 src="<?php echo $news[$i]->photo_src; ?>"></a></div>
 						<div class=cl><a target="_blank" href="/show/news/news.php?id=<?php echo $news[$i]->id; ?>">·<?php echo $news[$i]->title; ?></a></div>
 						<?php } ?>
 					</div>
