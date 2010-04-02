f4262ce8ebad2547dd7d6edfb51d9a6cd14e11cd
robbin17 <robbin_szf@hotmail.com>
2010-04-02 23:11:39 +0800

    robbin

--- fqzd/showindex.post.php
+++ fqzd/showindex.post.php
@@ -1,6 +1,7 @@
 ﻿<?php 
 	require_once('../frame.php');
 	$db=get_db();
+	$db->execute('update zd_question set show_index=0');
 	$db->execute('update zd_question set show_index=1 where id='.$_POST['id']);
 	echo "OK";
 ?>
