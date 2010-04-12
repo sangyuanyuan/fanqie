4126abfb7b9ee7ed6138f0191b0f28901816a031
robbin17 <robbin_szf@hotmail.com>
2010-04-12 13:40:09 +0800

    robbin

--- act/activity.post.php
+++ act/activity.post.php
@@ -4,6 +4,6 @@ $db=get_db();
 $StrSql='insert into smg_activities_signup (name,dept_id,mobile,createtime,sex,activities_id) values ("'.$_POST['name'].'",'.$_POST['dept_id'].',"'.$_POST['phone'].'",now(),"'.$_POST['sex'].'",'.$_POST['activities_id'].')';
 $Record = $db->execute($StrSql) or die ("insert error");
 echo '<script language=javascript>alert("提交成功！")</script>';
-echo '<script language=javascript>window.location.href="/";</script>';
+echo '<script language=javascript>window.location.href="/act/list.php?id=2";</script>';
 exit;
 ?>
