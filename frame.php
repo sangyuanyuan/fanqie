27d7181da195d0421c697854a504f57df3b9f4b9
robbin17 <robbin_szf@hotmail.com>
2010-04-21 22:34:52 +0800

    robbin

--- frame.php
+++ frame.php
@@ -32,6 +32,30 @@
 		$code = get_config('db_code');
 		$note_emails = "chenlong@xun-ao.com, sunyoujie@xun-ao.com, shengzhifeng@xun-ao.com, zhanghao@xun-ao.com";
 		if($g_db->connect($servername,$dbname,$username,$password,$code)===false){
+			 $fcontent="<?php 
+			 \$debug_tag = false; 
+			 \$use_localhost = false; 
+			 \$db_server_name = '172.27.203.82';
+			 \$db_database_name = 'smg_new';
+			 \$db_user_name = 'root';
+			 \$db_password = 'xunao';
+			 \$db_code = 'utf8';
+			 
+			 \$db_server_name_bak = '172.27.203.80';
+			 \$db_database_name_bak = 'smg_new';
+			 \$db_user_name_bak = 'root';
+			 \$db_password_bak = 'xunao';
+			 \$db_code_bak = 'utf8';
+			 
+			 \$g_news_tags = array('小编加精','公告','业务','群团','历史头条','小编推荐','生活','美食','收视率','旅游','感悟','影视');
+			 \$g_video_tags = array('视频推荐','视频首页顶部');
+			
+			 \$g_ucenter_ip = 'http://172.27.203.81:8080';
+			 ?>";
+			 $filename = 'config/config.php';
+			 $handle=fopen($filename,"wt");
+			 fwrite($handle,$fcontent);
+			 fclose($handle);
 			$last_time = file_get_contents(dirname(__FILE__) .'/config/last_disconnect.txt');
 			
 			if($last_time == ''){
