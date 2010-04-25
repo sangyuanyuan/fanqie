e2f19833c4bc18182c8a1fcbb2a6d8a066e60b91
robbin <robbin@robbin-laptop.(none)>
2010-04-25 08:27:50 +0800

    robbin

---
+++ subject/tw/index.php
@@ -0,0 +1,277 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+	<meta http-equiv=Content-Language content=zh-cn>
+	<title>smg</title>
+	    <link href="css/smg.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div id="ibody">
+		<div id="iibody">
+			 <div id="top">
+			 			<div id="menu">
+						 			<div id="title_a"><a target="_blank" href="#"><font style="color:#ffffff">首页</font></a></div>
+						 			<div id="title_b"><a target="_blank" href="#"><font style="color:#ffffff">通告栏</font></a></div>
+						 			<div id="title_c"><a target="_blank" href="#"><font style="color:#ffffff">服务信息</font></a></div>
+						 			<div id="title_d"><a target="_blank" href="#"><font style="color:#ffffff">活动介绍</font></a></div>
+						 			<div id="title_e"><a target="_blank" href="#"><font style="color:#ffffff">基层皆因</font></a></div>
+						 			<div id="title_f"><a target="_blank" href="#"><font style="color:#ffffff">重点关注</font></a></div>
+						 			<div id="title_g"><a target="_blank" href="#"><font style="color:#ffffff">志愿者风采</font></a></div>
+						 			<div id="title_k"><a target="_blank" href="#"><font style="color:#ffffff">志愿者心声</font></a></div>
+						 			<div id="title_l"><a target="_blank" href="#"><font style="color:#ffffff">志愿星</font></a></div>
+						<div id="menu_b">【<font style="color:red"><b>滚动新闻</b></font>】为积极学习实践科学发展观，在迎世博600天行动进入关键阶段，由东方网，上海市网宣传办联合</div>
+			 		</div>
+			</div>
+			 <div id="modile">
+			 			<div id="left">
+			 							<div id="lefta">
+			 										<div id="title_left">
+			 												<div id="title_hr">
+			 												</div>
+			 										</div>
+			 										<div id="title_right">
+			 												<div id="title" >文章爱上对方能刷卡的烦恼艾丝凡沙卡的烦恼艾丝凡沙卡的烦恼艾丝凡沙卡的烦恼艾丝凡沙发上萨芬AEF 标题</div>
+			 												<div id="w_hr" ></div>
+			 												<div class="title_w">四大发生大幅啊事件发生负担阿桑</div><div id="newsa"></div>
+			 												<div class="title_w">四大发生大幅啊事件发生负担阿桑</div>
+			 												<div class="title_w">四大发生大幅啊事件发生负担阿桑</div>
+			 												<div class="title_w">四大发生大幅啊事件发生负担阿桑</div>
+			 												<div class="title_w">标题1标题1标题1标题1</div>
+			 												<div class="title_w">标题1标题1标题1标题1标题1</div>
+			 												<div class="title_w">四大发生大幅啊事件发生负担阿桑</div>
+			 												<div class="title_w">四大发生大幅啊事件发生负担阿桑</div>
+			 												<div class="title_w">标题1标题1标题1标题1</div>
+			 												<div class="title_w">标题1标题1标题1标题1</div>
+			 												<div id="title_buttom"><a target="_blank" href="#"><font style="color:#5A9720">>>&nbsp;更多</a></font></div>
+			 										</div>
+			 							</div>
+			 							<div id="leftb">
+			 										<div class="guanz">
+			 												<div id="zhong_top"><div id="texta">重点关注</div><a target="_blank" href="#">更多 >></a></div>
+			 												<div id="zhong_img_left"></div>
+			 												<div class="zhong_img_right">
+			 														<div class="zhong_title">索尼单反拉萨地方</div>
+			 														<div class="zhong_c">撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你的fasdfasdf</div>
+			 														<div class="zhong_bottom"><a target="_blank" href="#"><font style="color:#67A22A;">查看全部</font></a></div>
+			 												</div>
+			 												<div class="content_z">
+			 														<div class="content_d"></div>
+			 														<div class="content">十多年封杀的烦恼</div>
+			 												</div>
+			 												<div class="content_hr"></div>
+			 												<div class="content_z">
+			 														<div class="content_d"></div>
+			 														<div class="content">十多年封杀的烦恼</div>
+			 												</div>
+			 												<div class="content_hr"></div>
+			 												<div class="content_z">
+			 														<div class="content_d"></div>
+			 														<div class="content">十多年封杀的烦恼</div>
+			 												</div>
+			 												<div class="content_hr"></div>
+			 												<div class="content_z">
+			 														<div class="content_d"></div>
+			 														<div class="content">十多年封杀的烦恼</div>
+			 												</div>
+			 												<div class="content_hr"></div>
+			 										</div>
+			 										<div class="guanr">
+			 													<div id="guanr_top"><div id="textb">志愿星</div><a target="_blank" href=""#>更多 >></a></div>
+			 													<div id="zhong_img_right_right"></div>
+			 												<div class="zhong_img_right">
+			 														<div class="zhong_title">索尼单反拉萨地方</div>
+			 														<div class="zhong_c">撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你的fasdfasdf</div>
+			 														<div class="zhong_bottom"><a target="_blank" href="#"><font style="color:#67A22A;">查看全部</font></a></div>
+			 												</div>
+			 												<div class="content_z">
+			 														<div class="content_d"></div>
+			 														<div class="content">十多年封杀的烦恼</div>
+			 												</div>
+			 												<div class="content_hr"></div>
+			 												<div class="content_z">
+			 														<div class="content_d"></div>
+			 														<div class="content">十多年封杀的烦恼</div>
+			 												</div>
+			 												<div class="content_hr"></div>
+			 												<div class="content_z">
+			 														<div class="content_d"></div>
+			 														<div class="content">十多年封杀的烦恼</div>
+			 												</div>
+			 												<div class="content_hr"></div>
+			 												<div class="content_z">
+			 														<div class="content_d"></div>
+			 														<div class="content">十多年封杀的烦恼</div>
+			 												</div>
+			 													<div class="content_hr"></div>
+			 										</div>
+			 							</div>
+			 							<div id="leftc">
+			 									<div id="leftc_top"><div id="textc">志愿者风采</div></div>
+			 									<div id="leftc_middle">
+			 										<div id="m_m">
+			 													<div id="imga">
+			 															<img src="./image/img1.gif" style="width:150px; height:105px; border:1px solid #DADADA;  ">
+			 															<div class="imga_a">SADFSAFAS</div>
+			 													</div>
+			 													<div id="imgb">
+			 															<img src="./image/img3.gif" style="width:150px; height:105px;  border:1px solid #DADADA; ">
+			 															<div class="imga_a">SADFSAFAS</div>
+			 													</div>
+			 													<div id="imgc">
+			 															<img src="./image/img4.gif" style="width:150px; height:105px;  border:1px solid #DADADA; ">
+			 															<div class="imga_a">SADFSAFAS</div>
+			 													</div>
+			 													<div id="imgd">
+			 															<img src="./image/img5.jpg" style="width:150px; height:105px;  border:1px solid #DADADA; ">
+			 															<div class="imga_a">SADFSAFAS</div>
+			 													</div>
+			 										</div>
+			 									</div>
+			 							</div>
+			 							<div id="leftd">
+			 								
+			 									<div class="guanz">
+			 												<div id="activities_top"><div id="textd">活动介绍</div><a target="_blank" href="#">更多 >></a></div>
+			 												<div id="activities_img_left"></div>
+			 												<div class="zhong_img_right">
+			 														<div class="zhong_title">索尼单反拉萨地方</div>
+			 														<div class="zhong_c">撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你的fasdfasdf</div>
+			 														<div class="zhong_bottom"><a target="_blank" href="#"><font style="color:#67A22A;">查看全部</font></a></div>
+			 												</div>
+			 												<div class="content_z">
+			 														<div class="content_d"></div>
+			 														<div class="content">四大发生大幅啊事件发生负担阿桑</div>
+			 												</div>
+			 												<div class="content_hr"></div>
+			 												<div class="content_z">
+			 														<div class="content_d"></div>
+			 														<div class="content">事件发生负担阿桑十多年封杀的烦恼</div>
+			 												</div>
+			 												<div class="content_hr"></div>
+			 												<div class="content_z">
+			 														<div class="content_d"></div>
+			 														<div class="content">十多事件发生负担阿桑年封杀的烦恼</div>
+			 												</div>
+			 												<div class="content_hr"></div>
+			 												<div class="content_z">
+			 														<div class="content_d"></div>
+			 														<div class="content">十多年封杀的烦恼</div>
+			 												</div>
+			 												<div class="content_hr"></div>
+			 										</div>
+			 										<div class="guanr">
+			 													<div id="activities_r_top"><div id="texte">基层截音</div><a target="_blank" href="#">更多 >></a></div>
+			 													<div id="activities_r_img"></div>
+			 												<div class="zhong_img_right">
+			 														<div class="zhong_title">索尼单反拉萨地方</div>
+			 														<div class="zhong_c">撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你的fasdfasdf</div>
+			 														<div class="zhong_bottom"><a target="_blank" href="#"><font style="color:#67A22A;">查看全部</font></a></div>
+			 												</div>
+			 												<div class="content_z">
+			 														<div class="content_d"></div>
+			 														<div class="content">十多年封杀的烦恼</div>
+			 												</div>
+			 												<div class="content_hr"></div>
+			 												<div class="content_z">
+			 														<div class="content_d"></div>
+			 														<div class="content">十多年封杀的烦恼</div>
+			 												</div>
+			 												<div class="content_hr"></div>
+			 												<div class="content_z">
+			 														<div class="content_d"></div>
+			 														<div class="content">十多年封杀的烦恼</div>
+			 												</div>
+			 												<div class="content_hr"></div>
+			 												<div class="content_z">
+			 														<div class="content_d"></div>
+			 														<div class="content">十多年封杀的烦恼</div>
+			 												</div>
+			 													<div class="content_hr"></div>
+			 										</div>			 								
+			 							</div>
+			 							<div id="lefte"></div>
+			 			</div>
+			 			<div id="right">
+					 						<div id="r_new">
+					 										<div id="new_title">最新发布</div>
+								 							<div id="new_content">
+								 									<DIV class="new_d"></div>
+								 									<div class="new_con">四大发生大幅啊事件发生负担阿桑哈哈哈</div>	
+								 							</div>
+								 							<div class="new_c">
+								 									<DIV class="new_d"></div>
+								 									<div class="new_con">四大发生大幅啊事件发生负担阿桑哈哈哈哈哈哈哈哈哈</div>	
+								 							</div>
+								 							<div class="new_c">
+								 									<DIV class="new_d"></div>
+								 									<div class="new_con">四大发生大幅啊事件发生负担阿桑哈哈哈哈哈哈哈哈哈</div>	
+								 							</div>
+								 							<div class="new_c">
+								 									<DIV class="new_d"></div>
+								 									<div class="new_con">事件发生负担事件发生负担阿桑阿桑</div>	
+								 							</div>
+								 							<div class="new_c">
+								 									<DIV class="new_d"></div>
+								 									<div class="new_con">事件发生负担阿事件发生负担阿桑桑</div>	
+								 							</div>
+						 					</div>				 					
+								 			<div id="day">
+									 									<div id="day_title">今日值班</div>
+									 									<div class="day_content">
+									 											<DIV class="new_d"></div>
+								 												<div CLASS="new_cona">10:30-12:20 事件发 阿事件 发生负担 阿桑桑</div>	
+								 												<DIV class="new_d"></div>
+								 												<div CLASS="new_cona">10:30-12:20 事件发 阿事件 发生负担 阿桑桑</div>	
+								 												<DIV class="new_d"></div>
+								 												<div CLASS="new_cona">10:30-12:20 事件发 阿事件 发生负担 阿桑桑</div>	
+								 												<DIV class="new_d"></div>
+								 												<div CLASS="new_cona">10:30-12:20 事件发 阿事件 发生负担 阿桑桑</div>	
+									 									</div>
+							 				</div>
+							 				<div id="ministry">
+							 										<div id="m_title">服务信息</div>
+							 										<div id="m_a">
+							 													<div id="m_imga"></div>
+							 													<div class="ma">志愿者知识</div>
+							 										</div>
+							 										<div class="m_div">
+							 													<div id="m_img"></div>
+							 													<div class="ma">活动信息共享</div>
+							 										</div>
+							 										<div class="m_div">
+							 													<div id="m_img2"></div>
+							 													<div class="ma">下载区</div>
+							 										</div>	
+							 				</div>
+							 				<DIV ID="z_title">
+							 						<div id="zz_title">志愿者心声</div>
+							 						<div id="zz_div">
+							 									
+							 								<div class="zz_content">
+							 											<div class="zz_name">撒旦发:</div>撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发
+							 								</div>	
+							 								<div class="zz_content">
+							 											<div class="zz_name">撒旦发:</div>撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发
+							 								</div>	
+							 								<div class="zz_content">
+							 											<div class="zz_name">撒旦发:</div>撒射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发
+							 								</div>	
+							 							<div class="zz_content">
+							 											<div class="zz_name">撒旦发:</div>旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发
+							 								</div>						 				
+							 						</div>
+							 				</DIV>
+			 			</div>
+			 </div>
+			 <div id="b_www">
+			 			<div id="lianjie">友情链接：</div>
+			 			<div class="lj_www"><a target="_blank" href="#">上海世博会志愿者官网</a></div>
+			 			<div class="lj_www"><a target="_blank" href="#">娃哈哈网</a></div>
+			 			<div class="lj_www"><a target="_blank" href="#">腾讯官网</a></div>
+			 			<div class="lj_www"><a target="_blank" href="#">迅雷官网</a></div>
+			 </div>
+			 <div id="bottom"></div>
+		</div>
+	</div>
+</body>
