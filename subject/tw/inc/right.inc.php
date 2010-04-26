99aee6fa6e903074c89603dbde1f1b874ff5f7a3
robbin <robbin@robbin-laptop.(none)>
2010-04-26 16:39:49 +0800

    robbin

--- subject/tw/inc/right.inc.php
+++ subject/tw/inc/right.inc.php
@@ -1,55 +1,50 @@
 <div id="right">
  <div id="r_new">
  					<div id="new_title">最新发布</div>
- 					
+ 					<?php $newnews = $db->query('select n.photo_src,n.id,n.short_title,n.news_type,n.file_name,n.category_id as cid from smg_subject_items i left join smg_news n on i.resource_id=n.id left join smg_subject_category c on c.id=i.category_id left join smg_subject s on c.subject_id=s.id where s.id=33 and i.category_type="news" and i.is_adopt=1 order by i.priority asc, n.created_at desc limit 5');?>
  					<div id="new_content">
  							<DIV class="new_d"></div>
- 							<div class="new_con"><a target="_blank" href="">四大发生大幅啊事件发生负担阿桑哈哈哈</a></div>	
+ 							<div class="new_con"><a target="_blank" href="news.php?id=<?php echo $newnews[0]->id; ?>"><?php echo $newnews[0]->short_title; ?></a></div>	
  					</div>
  					<?php for($i=1; $i<5; $i++){ ?>
- 					<div class="new_c">
- 							<DIV class="new_d"></div>
- 							<div class="new_con"><a target="_blank" href="">四大发生大幅啊事件发生负担阿桑哈哈哈哈哈哈哈哈哈</a></div>	
- 					</div>
+	 					<div class="new_c">
+	 							<DIV class="new_d"></div>
+	 							<div class="new_con"><a target="_blank" href="news.php?id=<?php echo $newnews[$i]->id; ?>"><?php echo $newnews[$i]->short_title; ?></a></div>	
+	 					</div>
  					<?php }?>
  	</div>				 					
  	<div id="day">
  								<div id="day_title">今日值班</div>
  								<div class="day_content">
+ 									<marquee width=100% height=100% DIRECTION="up" scrollamount="2" onmouseover=this.stop() onmouseout=this.start()>
  										<?php $day=$db->query('select id,content from smg_news where id=46862');?>
  										<a target="_blank" href="../news.php?id=<?php echo $day[0]->id; ?>"><?php echo get_fck_content($day[0]->content);?></a>
+ 									</marquee>
  								</div>
  	</div>
  	<div id="ministry">
  							<div id="m_title">服务信息</div>
  							<div id="m_a">
  										<div id="m_imga"></div>
- 										<div class="ma">志愿者知识</div>
+ 										<div class="ma"><a target="_blank" href="">志愿者知识</a></div>
  							</div>
  							<div class="m_div">
  										<div id="m_img"></div>
- 										<div class="ma">活动信息共享</div>
+ 										<div class="ma"><a target="_blank" href="">活动信息共享</a></div>
  							</div>
  							<div class="m_div">
  										<div id="m_img2"></div>
- 										<div class="ma">下载区</div>
+ 										<div class="ma"><a target="_blank" href="">下载区</a></div>
  							</div>	
  	</div>
  	<DIV ID="z_title">
  			<div id="zz_title">志愿者心声</div>
- 			<div id="zz_div">	
+ 			<div id="zz_div">
+ 				<?php for($i=0;$i<4;$i++){ ?>
  					<div class="zz_content">
  								<div class="zz_name">撒旦发:</div>撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发
- 					</div>	
- 					<div class="zz_content">
- 								<div class="zz_name">撒旦发:</div>撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发
- 					</div>	
- 					<div class="zz_content">
- 								<div class="zz_name">撒旦发:</div>撒射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发
- 					</div>	
- 					<div class="zz_content">
- 								<div class="zz_name">撒旦发:</div>旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发撒旦发射点发
- 					</div>						 				
+ 					</div>
+ 				<?php }?>						 				
  			</div>
  	</DIV>
  </div>
--- subject/tw/index.php
+++ subject/tw/index.php
@@ -18,7 +18,7 @@
 			 							<div id="lefta">
 			 										<div id="title_left">
 			 										<?php $sql = 'select n.photo_src,n.id,n.short_title,n.news_type,n.target_url,n.file_name,n.category_id as cid from smg_subject_items i left join smg_news n on i.resource_id=n.id left join smg_subject_category c on c.id=i.category_id left join smg_subject s on c.subject_id=s.id where s.id=33 and i.category_type="news" and i.is_adopt=1 and c.name="首页头新闻" order by i.priority asc, n.created_at desc limit 11';
-														  $record_star=$db -> query($sql);	 
+														  $record_star=$db -> query($sql); 
 													?>
 			 												<script src="/flash/sohuflash_1.js" type="text/javascript"></script>
 																<div id="focus_01"></div> 
@@ -63,40 +63,46 @@
 			 												<div id="title_right_title"><a target="_blank" href="news.php?id=<?php echo $record_star[0]->id; ?>"><?php echo $record_star[0]->short_title; ?></a></div>
 			 												<div id="w_hr" ></div>
 			 												<?php for($i=1;$i<11; $i++){ ?>
-			 												<div class="title_w"><a target="_blank" href="">四大发生大幅啊事件发生负担阿桑</a></div><?php if($i==1){ ?><div id="newsa"></div><?php }?>
+			 												<div class="title_w"><a target="_blank" href="news.php?id=<?php echo $record_star[$i]->id; ?>"><?php echo $record_star[$i]->short_title; ?></a></div><?php if($i==1){ ?><div id="newsa"></div><?php }?>
 			 												<?php }?>
-			 												<div id="title_buttom"><a target="_blank" href="#"><font style="color:#5A9720">>>&nbsp;更多</a></font></div>
+			 												<div id="title_buttom"><a target="_blank" href="list.php?id=175"><font style="color:#5A9720">>>&nbsp;更多</a></font></div>
 			 										</div>
 			 							</div>
 			 							<div id="leftb">
 			 										<div class="guanz">
-			 												<div id="zhong_top"><div id="texta">重点关注</div><a target="_blank" href="#">更多 >></a></div>
+			 										<?php $sql = 'select n.photo_src,n.id,n.short_title,n.description,n.news_type,n.target_url,n.file_name,n.category_id as cid from smg_subject_items i left join smg_news n on i.resource_id=n.id left join smg_subject_category c on c.id=i.category_id left join smg_subject s on c.subject_id=s.id where s.id=33 and i.category_type="news" and i.is_adopt=1 and c.name="重点关注" order by i.priority asc, n.created_at desc limit 6';
+														  $news=$db -> query($sql); 
+													?>
+			 												<div id="zhong_top"><div id="texta">重点关注</div><a target="_blank" href="list.php?id=162">更多 >></a></div>
 			 												<div id="zhong_img_left"></div>
 			 												<div class="zhong_img_right">
-			 														<div class="zhong_title">索尼单反拉萨地方</div>
-			 														<div class="zhong_c">撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你的fasdfasdf</div>
-			 														<div class="zhong_bottom"><a target="_blank" href="#"><font style="color:#67A22A;">查看全部</font></a></div>
+			 														<div class="zhong_title"><?php echo $news[0]->short_title; ?></div>
+			 														<div class="zhong_c"><?php echo $news[0]->description; ?></div>
+			 														<div class="zhong_bottom"><a target="_blank" href="news.php?id=<?php echo $news[0]->id; ?>"><font style="color:#67A22A;">查看全部</font></a></div>
 			 												</div>
 			 												<?php for($i=1; $i<5; $i++){ ?>
 			 												<div class="content_z">
 			 														<div class="content_d"></div>
-			 														<div class="content"><a target="_blank" href="">十多年封杀的烦恼</a></div>
+			 														<div class="content"><a target="_blank" href="news.php?id=<?php echo $news[$i]->id; ?>"><?php echo $news[$i]->short_title; ?></a></div>
 			 												</div>
 			 												<div class="content_hr"></div>
 			 												<?php }?>
 			 										</div>
 			 										<div class="guanr">
-			 													<div id="guanr_top"><div id="textb">志愿星</div><a target="_blank" href=""#>更多 >></a></div>
+				 										<?php $sql = 'select n.photo_src,n.id,n.short_title,n.description,n.news_type,n.target_url,n.file_name,n.category_id as cid from smg_subject_items i left join smg_news n on i.resource_id=n.id left join smg_subject_category c on c.id=i.category_id left join smg_subject s on c.subject_id=s.id where s.id=33 and i.category_type="news" and i.is_adopt=1 and c.name="志愿星" order by i.priority asc, n.created_at desc limit 6';
+															  $news=$db -> query($sql); 
+														?>
+			 													<div id="guanr_top"><div id="textb">志愿星</div><a target="_blank" href="list.php?id=16">更多 >></a></div>
 			 													<div id="zhong_img_right_right"></div>
 			 												<div class="zhong_img_right">
-			 														<div class="zhong_title">索尼单反拉萨地方</div>
-			 														<div class="zhong_c">撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你的fasdfasdf</div>
-			 														<div class="zhong_bottom"><a target="_blank" href="#"><font style="color:#67A22A;">查看全部</font></a></div>
+			 														<div class="zhong_title"><?php echo $news[0]->short_title; ?></div>
+			 														<div class="zhong_c"><?php echo $news[0]->description; ?></div>
+			 														<div class="zhong_bottom"><a target="_blank" href="news.php?id=<?php echo $news[0]->id; ?>"><font style="color:#67A22A;">查看全部</font></a></div>
 			 												</div>
 			 												<?php for($i=1; $i<5; $i++){ ?>
 				 												<div class="content_z">
 				 														<div class="content_d"></div>
-				 														<div class="content"><a target="_blank" href="">十多年封杀的烦恼</a></div>
+				 														<div class="content"><a target="_blank" href="news.php?id=<?php echo $news[$i]->id; ?>"><?php echo $news[$i]->short_title; ?></a></div>
 				 												</div>
 				 												<div class="content_hr"></div>
 			 												<?php }?>
@@ -106,6 +112,7 @@
 			 									<div id="leftc_top"><div id="textc">志愿者风采</div></div>
 			 									<div id="leftc_middle">
 			 										<div id="m_m">
+			 											
 			 													<div id="imga">
 			 															<img src="./image/img1.gif" style="width:150px; height:105px; border:1px solid #DADADA;  ">
 			 															<div class="imga_a">SADFSAFAS</div>
@@ -128,33 +135,39 @@
 			 							<div id="leftd">
 			 								
 			 									<div class="guanz">
-			 												<div id="activities_top"><div id="textd">活动介绍</div><a target="_blank" href="#">更多 >></a></div>
+			 											<?php $sql = 'select n.photo_src,n.id,n.short_title,n.description,n.news_type,n.target_url,n.file_name,n.category_id as cid from smg_subject_items i left join smg_news n on i.resource_id=n.id left join smg_subject_category c on c.id=i.category_id left join smg_subject s on c.subject_id=s.id where s.id=33 and i.category_type="news" and i.is_adopt=1 and c.name="活动介绍" order by i.priority asc, n.created_at desc limit 6';
+															  $news=$db -> query($sql); 
+														?>
+			 												<div id="activities_top"><div id="textd">活动介绍</div><a target="_blank" href="list.php?id=165">更多 >></a></div>
 			 												<div id="activities_img_left"></div>
 			 												<div class="zhong_img_right">
-			 														<div class="zhong_title">索尼单反拉萨地方</div>
-			 														<div class="zhong_c">撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你的fasdfasdf</div>
-			 														<div class="zhong_bottom"><a target="_blank" href="#"><font style="color:#67A22A;">查看全部</font></a></div>
+			 														<div class="zhong_title"><?php echo $news[0]->short_title; ?></div>
+			 														<div class="zhong_c"><?php echo $news[0]->description; ?></div>
+			 														<div class="zhong_bottom"><a target="_blank" href="news.php?id=<?php echo $news[0]->id; ?>"><font style="color:#67A22A;">查看全部</font></a></div>
 			 												</div>
 			 												<?php for($i=1;$i<5;$i++){ ?>
 			 												<div class="content_z">
 			 														<div class="content_d"></div>
-			 														<div class="content"><a target="_blank" href="">四大发生大幅啊事件发生负担阿桑</a></div>
+			 														<div class="content"><a target="_blank" href="news.php?id=<?php echo $news[$i]->id; ?>"><?php echo $news[$i]->short_title; ?></a></div>
 			 												</div>
 			 												<div class="content_hr"></div>
 			 												<?php }?>
 			 										</div>
 			 										<div class="guanr">
-			 													<div id="activities_r_top"><div id="texte">基层截音</div><a target="_blank" href="#">更多 >></a></div>
+			 											<?php $sql = 'select n.photo_src,n.id,n.short_title,n.description,n.news_type,n.target_url,n.file_name,n.category_id as cid from smg_subject_items i left join smg_news n on i.resource_id=n.id left join smg_subject_category c on c.id=i.category_id left join smg_subject s on c.subject_id=s.id where s.id=33 and i.category_type="news" and i.is_adopt=1 and c.name="基层撷音" order by i.priority asc, n.created_at desc limit 6';
+															  $news=$db -> query($sql); 
+														?>
+			 													<div id="activities_r_top"><div id="texte">基层撷音</div><a target="_blank" href="list.php?id=169">更多 >></a></div>
 			 													<div id="activities_r_img"></div>
 			 												<div class="zhong_img_right">
-			 														<div class="zhong_title">索尼单反拉萨地方</div>
-			 														<div class="zhong_c">撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你撒旦萨迪南磨房乐山大佛拉上你的fasdfasdf</div>
-			 														<div class="zhong_bottom"><a target="_blank" href="#"><font style="color:#67A22A;">查看全部</font></a></div>
+			 														<div class="zhong_title"><?php echo $news[0]->title; ?></div>
+			 														<div class="zhong_c"><?php echo $news[0]->description; ?></div>
+			 														<div class="zhong_bottom"><a target="_blank" href="news.php?id=<?php echo $news[0]->id; ?>"><font style="color:#67A22A;">查看全部</font></a></div>
 			 												</div>
 			 												<?php for($i=1;$i<5;$i++){ ?>
 			 												<div class="content_z">
 			 														<div class="content_d"></div>
-			 														<div class="content"><a target="_blank" href="">十多年封杀的烦恼</a></div>
+			 														<div class="content"><a target="_blank" href="news.php?id=<?php echo $news[$i]->id; ?>"><?php echo $news[$i]->short_title; ?></a></div>
 			 												</div>
 			 												<div class="content_hr"></div>
 			 												<?php }?>
