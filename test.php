059e8f36315291af6bbd9b08e79b86847e061b17
unknown <Administrator@.(none)>
2010-05-19 16:07:53 +0800

    robbin

--- test.php
+++ test.php
@@ -1,11 +1,12 @@
-<?php
+﻿<?php
 require_once('frame.php');
 $db=get_db();
-$sql="select * from smg_user where password=''";
-$up=$db->query($sql);
-for($i=0;$i<count($up);$i++)
-{
-	$sql="update smg_user set password=(select distinct(password) from smg_user_real where loginname='".$up[$i]->name."') where name='".$up[$i]->name."'";
-	$db->execute($sql);	
-}
+$ret = $db->query("show slave status");
+$db->move_first();
+#var_dump($ret);
+$io_status =  $db->field_by_index(10);
+$sql_status = $db->field_by_index(11);
+$syn_status = $io_status == 'Yes' &&  $sql_status == 'Yes';
+#var_dump($db);
+#send_sms('13482678134','东方时代');
 ?>
