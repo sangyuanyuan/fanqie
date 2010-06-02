098c0746d0c220f79f7bea4de2747ee573b3fef7
robbin17 <robbin_szf@hotmail.com>
2010-06-02 10:13:27 +0800

    robbin

--- show/article.php
+++ show/article.php
@@ -270,9 +270,9 @@
 			<div class=comment_box style="width:320px;">
 				<form id="showquestion" action="question.post.php" method="post">
 					<div class=c_title>现在有<span style="color:#FF5800"><?php echo count($question);?></span>人提问</div>
-					<div class=commenter_box><input type="text" style="width:170px;" id="c_n_n" name="post[name]"></div>
+					<div class=commenter_box style="width:310px;"><input type="text" style="width:170px;" id="c_n_n" name="post[name]"></div>
 					<input type="hidden" name="post[news_id]" value="<?php echo $id;?>">
-					<div class="commit_fck"><?php show_fckeditor('post[content]','Title',false,'75','','300');?></div>
+					<div class="commit_fck" style="width:310px;"><?php show_fckeditor('post[content]','Title',false,'75','','300');?></div>
 					<div id=submit_question></div>
 				</form>
 			</div>
