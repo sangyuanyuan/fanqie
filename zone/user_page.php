b0c076ced9fb95967e58f41e35b5078df7750e54
sauger <sauger@163.com>
2010-06-03 17:43:55 +0800

    fix bugs

--- zone/user_page.php
+++ zone/user_page.php
@@ -15,7 +15,14 @@
 		include_once '../lib/xspace_api.php';
 		include_once '../lib/uchome_api.php';
 		$db=get_db();
-		$items = get_user_models(160);
+		$user_id = intval($_COOKIE['smg_user_id']);
+		if($user_id <= 0){
+			alert('请先登录!');
+			redirect('/login/login.php');
+			die();
+		}
+		
+		$items = get_user_models($user_id);
   ?>
 <script>
 	total("交流首页","zone");
