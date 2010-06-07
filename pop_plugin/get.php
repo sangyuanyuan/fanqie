e89772c349165a4036e09f70f999d7177021222b
sauger <sauger@163.com>
2010-06-07 10:24:36 +0800

    add pop plugin

---
+++ pop_plugin/get.php
@@ -0,0 +1,11 @@
+<?php
+include '../frame.php';
+$id = intval($_GET['id']);
+$user = $_GET['user'];
+if(!$user || $id <= 0) die();
+$db = get_db();
+$db->query("select content from smg_pop_task where id={$id}");
+if($db->record_count <=0 ) die();
+$content = $db->field_by_name('content');
+$db->execute("insert into smg_pop_history (created_at,task_id,user) values (now(),{$id},'{$user}') ");
+echo $content;
---
+++ pop_plugin/query.php
@@ -0,0 +1,18 @@
+<?php
+include "../frame.php";
+$user = $_GET['user'];
+if(!$user) die();
+$db = get_db();
+$db->query("SELECT group_concat(task_id SEPARATOR \",\") as ids FROM smg_pop_history where user='{$user}' and created_at >= '" . date('Y-m-d')."'");
+if($db->field_by_name('ids')){
+	$read_ids = "(" . $db->field_by_name('ids')  .")";
+}
+$sql = "select id,width,height from smg_pop_task where created_at >='" .date('Y-m-d') ."'";
+if($read_ids){
+	$sql .= " and id not in {$read_ids}";
+}
+$sql .= " order by created_at asc limit 1";
+$item = $db->query($sql);
+if($db->record_count>0){
+	echo $item[0]->id ."|" .$item[0]->width ."|" .$item[0]->height; 
+}
